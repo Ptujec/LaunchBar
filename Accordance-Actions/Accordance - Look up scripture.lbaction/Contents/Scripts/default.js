3b2ea523e9c303f9b0c4bb857304cf6b69ff57e5
Ptujec <derbenderchristian@gmail.com>
2021-04-23 16:44:23 +0200

    Updated with UI language check
    I also built in a script that can translate input that Accordance does not understand. In this version it is disabled. You will need to enable it in default.js and adjust sloTitles.applescript. (As is the AppleScript will translate from Slovene to English book titles.)

--- Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
@@ -1,6 +1,26 @@
 // 2021-04-22 @Ptujec 
+// https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
 // http://www.accordancebible.com/Accordance-1043-Is-Automagical/
+// http://accordancefiles2.com/helpfiles/OSX12/Default.htm#topics/05_dd/using_links_common_tasks.htm#kanchor184 (See: Examples of Accordance-specific URLs)
+// https://stackoverflow.com/a/13012698 (if contains statement)
 
 function run(argument) {
-    LaunchBar.openURL('accord://read/' + encodeURIComponent(argument))
+    // next line is only relevant for Slovene input … maybe you can use it for another language … you will need to uncomment it
+    // argument = LaunchBar.executeAppleScriptFile('./sloTitles.applescript', argument);
+    
+    argument = argument.trim()
+    
+    if (LaunchBar.options.commandKey) {
+        LaunchBar.openURL('accord://read/' + encodeURIComponent(argument))
+    } else {
+        // UI language check
+        var lang = LaunchBar.executeAppleScriptFile('./langCheck.applescript');
+        if (lang.indexOf('de') >= 0) {
+            var allTextSetting = '[Alle_Texte];Verses?'
+        } else {
+            var allTextSetting = '[All_Texts];Verses?'
+        }
+
+        LaunchBar.openURL('accord://research/' + allTextSetting + encodeURIComponent(argument))
+    }
 }
--- Accordance-Actions/README.md
+++ Accordance-Actions/README.md
@@ -17,7 +17,7 @@ In order to make display and paste actions work properly it is suggested to set 
 - Display in large type
 - Paste scripture (with an option (⌘⏎) to paste as Markdown blockquote)
 - Look up scripture 
-- Search words
+- Search words *(I built in a script that can translate input that Accordance does not understand. In this version it is disabled. You will need to enable it in default.js and adjust sloTitles.applescript. (As is the AppleScript will translate from Slovene to English book titles.))*
 
 ## Download
 [Download LaunchBar Accordance Actions](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Accordance-Actions) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
