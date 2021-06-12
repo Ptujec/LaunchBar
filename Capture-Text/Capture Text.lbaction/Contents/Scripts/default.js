6c42bb8191333b8de6a8ce10cb1e5f352d113641
Ptujec <derbenderchristian@gmail.com>
2021-06-12 15:19:32 +0200

    New Action

---
+++ Capture-Text/Capture Text.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,102 @@
+// Image to Text (Christian Bender @ptujec)
+// using the great command line app macOCR by Marcus Schappi (@schappi)
+// https://github.com/schappim/macOCR
+
+// LaunchBar documentation
+// https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
+// https://www.obdev.at/resources/launchbar/help/URLCommands.html
+
+function run() {
+    var result = LaunchBar.execute('/usr/local/bin/ocr')
+        .trim()
+
+    if (LaunchBar.options.commandKey) {
+        LaunchBar.openURL('x-launchbar:select?string=' + encodeURI(result))
+
+    } else if (LaunchBar.options.shiftKey) {
+        // open URL, Email or Phone number
+
+        if (result.includes('www') || result.includes('http')) {
+            var url = result.replace(/www/, 'http://www')
+            LaunchBar.openURL(url)
+        } else if (result.includes('@')) {
+            var url = 'mailto:' + result
+            LaunchBar.openURL(url)
+        } else if (/\d\d\d/.test(result)) {
+            var url = 'tel:' + result.replace(/\D/g, '')
+            LaunchBar.openURL(url)
+        }
+
+    } else {
+
+        // Notification
+        // LaunchBar.displayNotification({
+        //     title: 'Text copied!',
+        //     string: result
+        // })
+
+        // Large display
+        var rLength = result.length
+
+        if (rLength > 80) {
+
+            var lineLength = rLength / 7
+
+            if (lineLength < 42) {
+                lineLength = 42
+            } else if (lineLength > 68) {
+                lineLength = 68
+            }
+
+            if (rLength > 948) {
+                // truncate 
+                result = result.substring(0, 949) + "…";
+                lineLength = 68
+            }
+
+            var arrayOfLines = fold(result, lineLength);
+            result = arrayOfLines.join('\n').replace(/\n\s/g, '\n')
+        }
+
+        LaunchBar.displayInLargeType({
+            title: 'Text copied!',
+            string: result
+        });
+
+        LaunchBar.execute('/usr/bin/afplay', '/System/Library/Components/CoreAudio.component/Contents/SharedSupport/SystemSounds/system/acknowledgment_sent.caf')
+
+        // Hide after 2 seconds
+        wait(2000);
+        LaunchBar.hide()
+    }
+}
+
+// time out routine
+// https://stackoverflow.com/a/33414145/15774924
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+
+// fold function
+// http://jsfiddle.net/jahroy/Rwr7q/18/
+function fold(s, n, a) {
+    a = a || [];
+    if (s.length <= n) {
+        a.push(s);
+        return a;
+    }
+    var line = s.substring(0, n);
+    var lastSpaceRgx = /\s(?!.*\s)/;
+    var idx = line.search(lastSpaceRgx);
+    var nextIdx = n;
+    if (idx > 0) {
+        line = line.substring(0, idx);
+        nextIdx = idx;
+    }
+    a.push(line);
+    return fold(s.substring(nextIdx), n, a);
+}
---
+++ Capture-Text/README.md
@@ -0,0 +1,29 @@
+# LaunchBar Action: Capture Text (powered by macOCR)
+
+<img src="capturetext.gif" width="600"/> 
+
+
+
+## Installation 
+This action is built upon the awesome command line app [macOCR](https://github.com/schappim/macOCR#readme) by  [Marcus Schappi](https://twitter.com/schappi). macOCR enables you to turn any text on your screen into text on your clipboard. **You have to [install macOCR](https://github.com/schappim/macOCR#installation) first to use the LaunchBar action.** 
+
+## Features
+**1) Capture & Copy:** 
+Simply run the action and select the area with the text you want to capture. Done. You will get a large display confirmation with the copied text, which will hide automatically after a few seconds. 
+
+<img src="textcopied.jpg" width="400"/> 
+
+**2) Capture & Select:**
+Hold command (⌘) while running the action to select the captured text in LaunchBar. 
+
+<img src="selected.jpg" width="400"/> 
+
+**3) Capture & Open:** 
+Hold shift (⇧) while running the action to automatically open captured links, email addresses or phone numbers.
+
+<img src="email.jpg" width="400"/> 
+
+
+## Download
+
+[Download LaunchBar Action: Capture Text](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Capture-Text) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
