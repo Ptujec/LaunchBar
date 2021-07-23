bafc2ce3e2201a58fa8da1cf220cc78fa4884168
Ptujec <derbenderchristian@gmail.com>
2021-07-23 21:27:31 +0200

    Updated Look Up action…
    - can handle more formats when using European Verse Notation (e.g. "1. Kor 1,1.13", "1Kor 1,1", "Apg 1,1; 3,1; 1Kor 13,13"…) even international formats like e.g. "Rom 1:1, 5"
    - should work better if you don't use European Verse Notation (basically everything Accordance can handle should work)
    - is smart about picking research or read view based on what you enter (e.g. picking research for single verses, read for chapters or if the entry contains "-" or ";")

--- Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
@@ -1,18 +1,118 @@
-// 2021-04-22 @Ptujec 
-// https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
-// http://www.accordancebible.com/Accordance-1043-Is-Automagical/
-// http://accordancefiles2.com/helpfiles/OSX12/Default.htm#topics/05_dd/using_links_common_tasks.htm#kanchor184 (See: Examples of Accordance-specific URLs)
-// https://stackoverflow.com/a/13012698 (if contains statement)
+/* Accordance Look Up by Ptujec 2021-07-22
+
+Sources: 
+- https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
+- http://www.accordancebible.com/Accordance-1043-Is-Automagical/
+- http://accordancefiles2.com/helpfiles/OSX12/Default.htm#topics/05_dd/using_links_common_tasks.htm#kanchor184 (See: Examples of Accordance-specific URLs)
+- https://stackoverflow.com/a/13012698 (if contains statement)
+*/
+
+const GermanBookList = ["Mose", "Genesis", "Exodus", "Levitikus", "Numeri", "Deuternomium", "Josua", "Richter", "Rut", "Könige", "Chronik", "Esra", "Nehemia", "Ester", "Hiob", "Psalmen", "Sprichwörter", "Sprüche", "Kohelet", "Prediger", "Hohelied", "Jesaja", "Jeremia", "Klagelieder", "Hesekiel", "Hosea", "Obadja", "Jona", "Micha", "Habakuk", "Zefanja", "Haggai", "Sacharja", "Maleachi", "Matthäus", "Markus", "Lukas", "Johannes", "Apg", "Apostelgeschichte", "Römer", "Korinther", "Galater", "Epheser", "Philipper", "Kolosser", "Thessalonicher", "Timotheus", "Philemon", "Hebräer", "Jakobus", "Petrus", "Judas", "Offenbarung"]
+
+const SloveneBookList = ["Mojzes", "Geneza", "Eksodus", "Levitik", "Numeri", "Devteronomij", "Jozue", "Sodniki", "Ruta", "Kralji", "Kroniška", "Ezra", "Nehemija", "Estera", "Job", "Psalmi", "Pregovori", "Pregovori", "Kohelet", "Pridigar", "Visoka pesem", "Izaija", "Jeremija", "Žalostinke", "Ezekiel", "Ozej", "Abdija", "Jona", "Mihej", "Habakuk", "Sofonija", "Agej", "Zaharija", "Malahija", "Matej", "Marko", "Luka", "Janez", "Apd", "Apostolska dela", "Rimljanom", "Korinčanom", "Galačanom", "Efežanom", "Filipljanom", "Kološanom", "Tesaloničanom", "Timoteju", "Filemonu", "Hebrejcem", "Jakob", "Peter", "Juda", "Razodetje"]
+
+const EnglishBookList = ["Moses", "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "Kings", "Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Proverbs", "Ecclesiastes", "Ecclesiastes", "Song", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Hosea", "Obadiah", "Jonah", "Micah", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Acts", "Romans", "Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "Thessalonians", "Timothy", "Philemon", "Hebrews", "James", "Peter", "Jude", "Revelation"]
+
 
 function run(argument) {
-    // next line is only relevant for Slovene input … maybe you can use it for another language … you will need to uncomment it
-    // argument = LaunchBar.executeAppleScriptFile('./sloTitles.applescript', argument);
-    
-    argument = argument.trim()
-    
-    if (LaunchBar.options.commandKey) {
-        LaunchBar.openURL('accord://read/' + encodeURIComponent(argument))
+    argument = argument
+        .trim()
+
+    // Check Vers Notation Setting (see checkbox in "Appearance" section of Accoradance Preferences)
+    var content = File.readText('~/Library/Preferences/Accordance Preferences/General.apref')
+    var string = content.match(/useeuropeanversenotation":(\d)/)
+    var num = string[1]
+
+    if (num == 0) {
+        // Default Vers Notation
+        var result = argument
     } else {
+        // European Vers Notation
+        argument = argument
+            // clean up capture (e.g. brackets) and formart errors (e.g. spaces before or after verse numbers) in entry
+            .replace(/\(|\)/g, '')
+            .replace(/(\s+)?([\-–,:])(\s+)?/g, '$2')
+
+        // Convert Slovene and German argument strings
+        var mA = argument.match(/(?:[1-5]\.?\s?)?(?:[a-zžščöäü]+\.?\s?)?[0-9,.:\-–f]+/gi)
+
+        var result = []
+        for (var i = 0; i < mA.length; i++) {
+            var scrip = mA[i]
+                .toString()
+                .trim()
+
+            // makes sure non-european styles get converted 
+            if (scrip.includes(':')) {
+                scrip = scrip
+                    .replace(/,/g, '.')
+                    .replace(/:/g, ',')
+            }
+
+            var mB = scrip.match(/([1-5]\.?\s?)?([a-zžščöäü]+\.?\s?)?([0-9,.:\-–f]+)/i)
+
+            var prefix = mB[1]
+
+            if (prefix == undefined) {
+                prefix = ''
+            } else {
+                prefix = prefix
+                    .replace(/\./, '')
+            }
+
+            var bookName = mB[2]
+
+            if (bookName == undefined) {
+                bookName = ''
+            } else {
+                bookName = bookName
+                    .trim()
+                    .replace(/\./, '')
+                    .toLowerCase()
+
+                var iBN = GermanBookList
+                    .findIndex(element => element
+                        .toLowerCase()
+                        .startsWith(bookName))
+
+
+                if (iBN == -1) {
+                    iBN = SloveneBookList
+                        .findIndex(element => element
+                            .toLowerCase()
+                            .startsWith(bookName))
+                }
+
+                if (iBN != -1) {
+                    bookName = EnglishBookList[iBN]
+                }
+                bookName = bookName + ' '
+            }
+            var suffix = mB[3]
+
+            var newScrip = prefix + bookName + suffix
+
+            result.push(newScrip + ' ')
+        }
+        result = result
+            .toString()
+            .replace(/ ,/g, '; ')
+            .trim()
+            .replace(/1 Moses|1Moses/, 'Genesis')
+            .replace(/2 Moses|2Moses/, 'Exodus')
+            .replace(/3 Moses|3Moses/, 'Leviticus')
+            .replace(/4 Moses|4Moses/, 'Numbers')
+            .replace(/5 Moses|5Moses/, 'Deuteronomy')
+
+    }
+    lookUp(result)
+}
+
+function lookUp(result) {
+
+    if (LaunchBar.options.commandKey) {
+        LaunchBar.openURL('accord://read/' + encodeURIComponent(result))
+    } else if (LaunchBar.options.alternateKey) {
         // UI language check
         var lang = LaunchBar.executeAppleScriptFile('./langCheck.applescript');
         if (lang.indexOf('de') >= 0) {
@@ -20,7 +120,21 @@ function run(argument) {
         } else {
             var allTextSetting = '[All_Texts];Verses?'
         }
-
-        LaunchBar.openURL('accord://research/' + allTextSetting + encodeURIComponent(argument))
+        LaunchBar.openURL('accord://research/' + allTextSetting + encodeURIComponent(result))
+    } else {
+        // Smart Option
+        if (result.includes('-') || result.includes(';') || !result.includes(',')) {
+            LaunchBar.openURL('accord://read/?' + encodeURIComponent(result))
+        } else {
+            // UI language check
+            var lang = LaunchBar.executeAppleScriptFile('./langCheck.applescript')
+                .trim()
+            if (lang.startsWith('de')) {
+                var allTextSetting = '[Alle_Texte];Verses?'
+            } else {
+                var allTextSetting = '[All_Texts];Verses?'
+            }
+            LaunchBar.openURL('accord://research/' + allTextSetting + encodeURIComponent(result))
+        }
     }
 }
