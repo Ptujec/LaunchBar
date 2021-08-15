2c675f98049e38563d95143cb57d4f9c1a0561b4
Ptujec <derbenderchristian@gmail.com>
2021-08-15 23:56:08 +0200

    Live Feedback and better error handling

--- Converter-Actions/DollarEuro.lbaction/Contents/Scripts/default.js
+++ Converter-Actions/DollarEuro.lbaction/Contents/Scripts/default.js
@@ -4,55 +4,86 @@
 // http://openexchangerates.github.io/accounting.js/
 // jquery - Convert String with Dot or Comma as decimal separator to number in JavaScript - Stack Overflow https://stackoverflow.com/a/29347112
 
-const apiKey = '123' // Replace 123 with your API Key in default.js (in LaunchBars Acticon Editor). You can get an API Key for free at https://exchangeratesapi.io/pricing/
-const exchangerate = HTTP.getJSON('http://api.exchangeratesapi.io/latest?access_key=' + apiKey + '&symbols=USD')
+const apiKey = Action.preferences.apiKey
 
 function run(argument) {
-    if (apiKey == '123') {
-        LaunchBar.alert('Replace 123 with your API Key in default.js (in LaunchBars Acticon Editor). You can get an API Key for free at https://exchangeratesapi.io/pricing/')
-    }
+    // Check for valid API Access Key
+    if (apiKey == undefined) {
+        setApiKey()
+    } else {
+        const exchangerate = HTTP.getJSON('http://api.exchangeratesapi.io/latest?access_key=' + apiKey + '&symbols=USD')
 
-    if (exchangerate.data.error != undefined) {
-        LaunchBar.alert('Unable to load results', exchangerate.data.error.info);
-    } else if (exchangerate.data != undefined) {
-        argument = argument.toString().replace(/[^0-9.,]*/g, '')
-
-        var result = argument.replace(/[^0-9]/g, '');
-        if (/[,\.]\d{2}$/.test(argument)) {
-            result = result.replace(/(\d{2})$/, '.$1');
+        if (exchangerate.error != undefined) {
+            LaunchBar.alert(exchangerate.error)
+            return
         }
 
-        argument = result
-        number = argument.replace('.', ',')
+        if (exchangerate.data.error != undefined) {
+            if (exchangerate.data.error.info.includes('You have not supplied a valid API Access Key.')) {
+                setApiKey()
+            } else {
+                LaunchBar.alert('Unable to load results', exchangerate.data.error.info);
+            }
 
-        var dollarRate = exchangerate.data.rates.USD
-        var euroRate = 1 / dollarRate
-        var euro = argument * euroRate
-        euro = euro.toFixed(2)
-        euro = euro.toString().replace('.', ',')
-        var dollar = argument * dollarRate
-        dollar = dollar.toFixed(2)
-        dollar = dollar.toString().replace('.', ',')
-        dollarRate = dollarRate.toFixed(2)
-        dollarRate = dollarRate.toString().replace('.', ',')
-        euroRate = euroRate.toFixed(2)
-        euroRate = euroRate.toString().replace('.', ',')
+            // Main action
+        } else if (exchangerate.data != undefined) {
+            argument = argument.toString().replace(/[^0-9.,]*/g, '')
 
-        var eResult = number + ' USD = ' + euro + ' EUR (Rate: ' + euroRate + ')'
-        var dResult = number + ' EUR = ' + dollar + ' USD (Rate: ' + dollarRate + ')'
-        LaunchBar.setClipboardString(eResult + '\n' + dResult)
+            var result = argument.replace(/[^0-9]/g, '');
+            if (/[,\.]\d{2}$/.test(argument)) {
+                result = result.replace(/(\d{2})$/, '.$1');
+            }
 
-        return [{
-            title: euro,
-            subtitle: number + ' USD (Rate: ' + euroRate + ')',
-            icon: "DollarEuroTemplate",
-            badge: 'EUR'
-        }, {
-            title: dollar,
-            subtitle: number + ' EUR (Rate: ' + dollarRate + ')',
-            icon: "EuroDollarTemplate",
-            badge: 'USD'
-        }]
+            argument = result
+            number = argument.replace('.', ',')
 
+            var dollarRate = exchangerate.data.rates.USD
+            var euroRate = 1 / dollarRate
+            var euro = argument * euroRate
+            euro = euro.toFixed(2)
+            euro = euro.toString().replace('.', ',')
+            var dollar = argument * dollarRate
+            dollar = dollar.toFixed(2)
+            dollar = dollar.toString().replace('.', ',')
+            dollarRate = dollarRate.toFixed(2)
+            dollarRate = dollarRate.toString().replace('.', ',')
+            euroRate = euroRate.toFixed(2)
+            euroRate = euroRate.toString().replace('.', ',')
+
+            var eResult = number + ' USD = ' + euro + ' EUR (Rate: ' + euroRate + ')'
+            var dResult = number + ' EUR = ' + dollar + ' USD (Rate: ' + dollarRate + ')'
+            LaunchBar.setClipboardString(eResult + '\n' + dResult)
+
+            return [{
+                title: euro,
+                subtitle: number + ' USD (Rate: ' + euroRate + ')',
+                icon: "DollarEuroTemplate",
+                badge: 'EUR'
+            }, {
+                title: dollar,
+                subtitle: number + ' EUR (Rate: ' + dollarRate + ')',
+                icon: "EuroDollarTemplate",
+                badge: 'USD'
+            }]
+
+        }
+    }
+}
+
+function setApiKey() {
+    var response = LaunchBar.alert(
+        "API Access Key required", "You can get a free API Access Key at https://exchangeratesapi.io/pricing/. Copy the key to your clipboard, run the action again and choose »Set API-Token«", "Open Website", "Set API-Token", "Cancel"
+    );
+    switch (response) {
+        case 0:
+            LaunchBar.openURL('https://exchangeratesapi.io/pricing/')
+            LaunchBar.hide()
+            break
+        case 1:
+            Action.preferences.apiKey = LaunchBar.getClipboardString().trim()
+            LaunchBar.alert('Success!', 'API Access Key set to: ' + Action.preferences.apiKey)
+            break
+        case 2:
+            break
     }
 }
