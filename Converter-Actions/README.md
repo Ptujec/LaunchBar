7e7ec00520a200d26fd7ddca648549e1bd260e6b
Ptujec <derbenderchristian@gmail.com>
2021-08-16 00:39:47 +0200

    Merged Temperatur Actions into one

--- Converter-Actions/README.md
+++ Converter-Actions/README.md
@@ -6,8 +6,7 @@
 ## List
 
 - Dollar ⇄ Euro (requires an API key. You can get one for free at [https://exchangeratesapi.io/pricing/](https://exchangeratesapi.io/pricing/))
-- Temperatur - Celsius in Fahrenheit
-- Temperatur - Fahrenheit in Celsius
+- Temperatur - Celsius ⇄ Fahrenheit
 
 ## Download
 [Download LaunchBar Converter Actions](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Converter-Actions) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
---
+++ Converter-Actions/Temperatur - Celsius ⇄ Fahrenheit.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,19 @@
+// Calculation Celius into Fahrenheit and vise versa
+
+function run(argument) {
+    let fahrenheit = (argument * 9 / 5) + 32;
+    let celsius = (argument - 32) * 5 / 9;
+
+    return [
+        {
+            title: fahrenheit.toFixed(1).replace('.', ',') + ' °F',
+            subtitle: argument.replace('.', ',') + ' °C',
+            icon: "ThermoTemplate"
+        },
+        {
+            title: celsius.toFixed(1).replace('.', ',') + ' °C',
+            subtitle: argument.replace('.', ',') + ' °F',
+            icon: "ThermoTemplate"
+        },
+    ];
+}
