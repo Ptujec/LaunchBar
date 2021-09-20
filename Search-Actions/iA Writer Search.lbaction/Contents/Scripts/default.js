bb939a141a3f2f77addf96b3d0a71aa35de50372
Ptujec <derbenderchristian@gmail.com>
2021-09-20 20:03:52 +0200

    Update

--- Search-Actions/iA Writer Search.lbaction/Contents/Scripts/default.js
+++ Search-Actions/iA Writer Search.lbaction/Contents/Scripts/default.js
@@ -1,9 +1,76 @@
-// https://ia.net/writer/support/general/urlschemes
+/* 
+iA Writer Search 
+by Ptujec 
+2021-07-13
+*/
 
 function run(argument) {
-    if (argument == undefined) {
-        LaunchBar.openURL('ia-writer://quick-search');
+    argument = argument
+        .toLowerCase()
+        .trim()
+
+    if (LaunchBar.options.commandKey) {
+        // Dateiname 
+        var output = LaunchBar.execute('/usr/bin/mdfind', '-onlyin', '/Users/hischa/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents', '-name', argument)
+            .split('\n')
+    } else if (LaunchBar.options.shiftKey) {
+        LaunchBar.performAction('Run Terminal Command', 'grep -ri ' + argument + ' /Users/hischa/Library/Mobile\\ Documents/27N4MQEA55~pro~writer/Documents')
+        return
     } else {
-        LaunchBar.openURL('ia-writer://quick-search?query=' + encodeURIComponent(argument));
+        var output = LaunchBar.execute('/usr/bin/mdfind', '-onlyin', '/Users/hischa/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents', argument)
+            .split('\n')
+    }
+
+    // LaunchBar.alert(output.length)
+    // return
+
+    if (output == '') {
+        return [{
+            'title': 'Keine Treffer',
+            'icon': 'pro.writer.mac'
+        }]
+    } else {
+        var results = []
+        for (var i = 0; i < output.length; i++) {
+            var result = output[i]
+            if (result != '' && !File.isDirectory(result)) {
+                var path = result
+
+                if (LaunchBar.options.commandKey) {
+                    results.push({
+                        'path': path
+                    })
+
+                } else {
+
+                    var regex = new RegExp('.*' + argument + '.*', 'gi')
+                    try {
+                        var sub = File.readText(path)
+                            .match(regex)
+                    } catch (error) {
+                    }
+
+                    if (sub != null) {
+                        sub = sub
+                            .toString()
+                            .replace(/\n/g, ' ')
+                            .trim()
+
+                        results.push({
+                            'subtitle': sub,
+                            'path': path
+                        })
+                    } else {
+                        results.push({
+                            'path': path
+                        })
+                    }
+                }
+            }
+        }
+        results.sort(function (a, b) {
+            return a.title > b.title;
+        });
+        return results
     }
 }
