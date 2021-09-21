3846a2aa67b523f51be0e1887c2c1740760f3d09
Ptujec <derbenderchristian@gmail.com>
2021-09-21 17:38:18 +0200

    Update
    - added support for folders
    - sorting
    - fix: encoding path

--- Obsidian-Actions/Obsidian - Vaults and Notes.lbaction/Contents/Scripts/default.js
+++ Obsidian-Actions/Obsidian - Vaults and Notes.lbaction/Contents/Scripts/default.js
@@ -9,34 +9,97 @@ function run() {
     var oJSON = File.readJSON('~/Library/Application Support/obsidian/obsidian.json')
     var vaults = Object.keys(oJSON.vaults)
 
-    var results = []
+    var vaultResults = []
     for (var i = 0; i < vaults.length; i++) {
 
         var vault = vaults[i]
-
-        var vPath = oJSON.vaults[vault].path
+        var vPath = oJSON.vaults[vault].path + '/'
         var vName = File.displayName(vPath)
 
-        results.push({
+        vaultResults.push({
             'title': vName,
             'icon': 'vaultTemplate.png',
             'action': 'setVault',
             'actionArgument': vPath
         })
     }
-    return results
+    vaultResults.sort(function (a, b) {
+        return a.title > b.title;
+    });
+    return vaultResults
 }
 
 function setVault(vPath) {
     var contents = File.getDirectoryContents(vPath);
+    var folders = []
     var files = []
     for (var i = 0; i < contents.length; i++) {
-        var file = contents[i]
-        files.push({
-            'title': file,
-            'icon': 'docTemplate',
-            'url': 'obsidian://open?path=' + vPath + file
-        })
+        var item = contents[i]
+
+        if (!item.includes('.')) {
+            var folder = item + '/'
+            folders.push({
+                'title': item,
+                'icon': 'folderTemplate',
+                'path': vPath + folder,
+                'action': 'openFolder',
+                'actionArgument': vPath + folder,
+                'type': '01'
+            })
+        } else {
+            var url = 'obsidian://open?path=' + encodeURI(vPath + item)
+            files.push({
+                'title': item,
+                'icon': 'docTemplate',
+                'url': url,
+                'type': '02'
+            })
+        }
     }
-    return files
+    folders.sort(function (a, b) {
+        return a.title > b.title;
+    });
+    files.sort(function (a, b) {
+        return a.title > b.title;
+    });
+    var items = folders.concat(files)
+    return items
+}
+
+function openFolder(fPath) {
+    var contents = File.getDirectoryContents(fPath);
+    var folders = []
+    var files = []
+    for (var i = 0; i < contents.length; i++) {
+        var item = contents[i]
+
+        if (!item.includes('.')) {
+            var folder = item + '/'
+            folders.push({
+                'title': item,
+                'icon': 'folderTemplate',
+                'path': fPath + folder,
+                'action': 'openFolder',
+                'actionArgument': fPath + folder,
+                'type': '01'
+            })
+        } else {
+            var url = 'obsidian://open?path=' + encodeURI(fPath + item)
+            files.push({
+                'title': item,
+                'icon': 'docTemplate',
+                'url': url,
+                'type': '02'
+            })
+        }
+
+    }
+    folders.sort(function (a, b) {
+        return a.title > b.title;
+    });
+    files.sort(function (a, b) {
+        return a.title > b.title;
+    });
+    var items = folders.concat(files)
+    return items
 }
