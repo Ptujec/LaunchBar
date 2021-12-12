e0b732fbe3a43b0b8b54df8e6e760baaf6437720
Ptujec <derbenderchristian@gmail.com>
2021-12-12 14:31:21 +0100

    Update: Folder support
    You can now limit the list of shortcuts to a specific folder (⌥⏎)

--- Run-Shortcut/README.md
+++ Run-Shortcut/README.md
@@ -3,7 +3,16 @@
 <img src="screenshot.png" width="600"/> 
 
 This action lists all your shortcuts (macOS 12 required). Select one (with arrow keys or by typing) and hit return to run it.
-This action is associated with the Shortcuts app. This means you also get the list if you hit right arrow or space when the app is selected in LaunchBar.  
+
+<img src="screenshot_02.png" width="600"/> 
+
+You can limit the list of shortcuts to a specific folder, which you can select by pressing ⌥⏎.
+
+<img src="screenshot_03.png" width="600"/> 
+
+
+(This action is associated with the Shortcuts app. This means you also get the list, if you hit right arrow or space, when the app is selected in LaunchBar.)
+ 
 
 ## Download
 
--- Run-Shortcut/Run Shortcut.lbaction/Contents/Scripts/default.js
+++ Run-Shortcut/Run Shortcut.lbaction/Contents/Scripts/default.js
@@ -6,26 +6,131 @@ Documentation:
 - https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
 */
 
-
 function run() {
-    var output = LaunchBar.execute('/usr/bin/shortcuts', 'list')
-        .trim()
-        .split('\n')
+    if (LaunchBar.options.alternateKey) {
+        var folderList = LaunchBar.execute('/usr/bin/shortcuts', 'list', '--folders')
+            .trim()
+            .split('\n')
 
-    var result = []
-    for (var i = 0; i < output.length; i++) {
-        result.push({
-            'title': output[i],
-            'icon' : 'shortcutTemplate.png',
-            'action' : 'runShortcut',
-            'actionArgument' : output[i],
-            'actionRunsInBackground': true
-        })
+        if (Action.preferences.folder != undefined) {
+            var selectedFolder = Action.preferences.folder
+        } else {
+            var selectedFolder = 'all'
+        }
+
+        var folders = []
+        for (var i = 0; i < folderList.length; i++) {
+
+            if (selectedFolder == folderList[i]) {
+                var icon = 'selectedFolderTemplate'
+            } else {
+                var icon = 'folderTemplate'
+            }
+
+            folders.push({
+                'title': folderList[i],
+                'icon': icon,
+                'action': 'setFolder',
+                'actionArgument': folderList[i],
+            })
+        }
+        folders.sort(function (a, b) {
+            return a.title > b.title;
+        });
+
+        if (LaunchBar.currentLocale == 'de') {
+            var aS = 'Alle Shortcuts'
+        } else {
+            var aS = 'All Shortcuts'
+        }
+
+        if (selectedFolder == 'all') {
+            var allIcon = 'selectedFolderTemplate'
+        } else {
+            var allIcon = 'folderTemplate'
+        }
+
+        var all = [{
+            'title': aS,
+            'icon': allIcon,
+            'action': 'setFolder',
+            'actionArgument': 'all',
+        }]
+        var result = all.concat(folders)
+        return result
+    } else {
+        var shortcuts = showList()
+        return shortcuts;
     }
-    result.sort(function (a, b) {
+}
+
+function setFolder(f) {
+    Action.preferences.folder = f
+    var shortcuts = showList()
+    return shortcuts;
+}
+
+function showList() {
+    var shortcuts = []
+
+    if (LaunchBar.currentLocale == 'de') {
+        var aS = 'Alle Shortcuts'
+    } else {
+        var aS = 'All Shortcuts'
+    }
+
+    if (Action.preferences.folder == undefined) {
+        var list = LaunchBar.execute('/usr/bin/shortcuts', 'list')
+            .trim()
+            .split('\n')
+
+        for (var i = 0; i < list.length; i++) {
+            shortcuts.push({
+                'title': list[i],
+                'icon': 'shortcutTemplate.png',
+                'action': 'runShortcut',
+                'actionArgument': list[i],
+                'actionRunsInBackground': true
+            })
+        }
+    } else {
+        var selectedFolder = Action.preferences.folder
+        if (selectedFolder == 'all') {
+            var list = LaunchBar.execute('/usr/bin/shortcuts', 'list')
+                .trim()
+                .split('\n')
+
+            for (var i = 0; i < list.length; i++) {
+                shortcuts.push({
+                    'title': list[i],
+                    'icon': 'shortcutTemplate.png',
+                    'action': 'runShortcut',
+                    'actionArgument': list[i],
+                    'actionRunsInBackground': true
+                })
+            }
+        } else {
+            var list = LaunchBar.execute('/usr/bin/shortcuts', 'list', '--folder-name', selectedFolder)
+                .trim()
+                .split('\n')
+
+            for (var i = 0; i < list.length; i++) {
+                shortcuts.push({
+                    'title': list[i],
+                    'icon': 'shortcutTemplate.png',
+                    'badge': selectedFolder,
+                    'action': 'runShortcut',
+                    'actionArgument': list[i],
+                    'actionRunsInBackground': true
+                })
+            }
+        }
+    }
+
+    shortcuts.sort(function (a, b) {
         return a.title > b.title;
     });
-    return result
+    return shortcuts
 }
 
 function runShortcut(s) {
