d9df3279c9547e65793aee8961ae8a92174f0f68
Ptujec <derbenderchristian@gmail.com>
2021-12-31 17:30:08 +0100

    Update

--- Search-Actions/Zotero Search.lbaction/Contents/Scripts/content.applescript
+++ Search-Actions/Zotero Search.lbaction/Contents/Scripts/content.applescript
@@ -3,30 +3,9 @@
 
 on run (_arguments)
 	set thequery to _arguments as string
-	try
 		set contentQueryString to "kMDItemTextContent  == " & quote & thequery & "*" & quote & "wc "
 		set endQueryString to "'( " & contentQueryString & " )'"
 		set thecmd to "mdfind -onlyin ~/Zotero/storage  " & endQueryString
 		set theresult to do shell script thecmd & " grep"
-		set thelist to {}
-		set allparas to every paragraph in theresult
-		set thelist to theresult
-		if thelist is "" then
-			tell application "LaunchBar"
-				display in large type "Nothing found!" with sound "Submarine"
-				delay 0.7
-				hide
-			end tell
-		else
-			tell application "LaunchBar"
-				set selection to thelist
-				activate
-			end tell
-		end if
-	on error e
-		tell application "BBEdit"
-		make new document with properties {name:"Zotero Search Error", text:e}
-		activate
-		end tell
-	end try
+		return theresult
 end run
--- Search-Actions/Zotero Search.lbaction/Contents/Scripts/default.js
+++ Search-Actions/Zotero Search.lbaction/Contents/Scripts/default.js
@@ -1,10 +1,27 @@
 function run(argument) {
     if (LaunchBar.options.commandKey) {
-        LaunchBar.executeAppleScriptFile('./content.applescript', argument);
+        var output = LaunchBar.executeAppleScriptFile('./content.applescript', argument)
+            .trim()
+            .split('\r')
     } else {
-        var output = LaunchBar.executeAppleScriptFile('./default.applescript', argument);
-        if (output == "\n") {
-            LaunchBar.executeAppleScriptFile('./content.applescript', argument);
+        var output = LaunchBar.executeAppleScriptFile('./default.applescript', argument)
+            .trim()
+            .split('\r')
+
+        if (output == '') {
+            output = LaunchBar.executeAppleScriptFile('./content.applescript', argument)
+                .trim()
+                .split('\r')
         }
     }
+
+    var result = []
+    for (var i = 0; i < output.length; i++) {
+        result.push({
+            path: output[i]
+        })
+    }
+
+    return result
+
 }
