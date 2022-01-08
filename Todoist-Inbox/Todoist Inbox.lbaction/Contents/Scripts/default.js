b863b0ac8716480431cb917c386a6820d9d507c6
Ptujec <derbenderchristian@gmail.com>
2022-01-09 00:08:01 +0100

    Update 1.1
    - Advanced Options can also be triggered by shit-enter

--- Todoist-Inbox/Todoist Inbox.lbaction/Contents/Scripts/default.js
+++ Todoist-Inbox/Todoist Inbox.lbaction/Contents/Scripts/default.js
@@ -129,7 +129,7 @@ function run(argument) {
         lang: lang,
       };
 
-      if (advanced == true) {
+      if (advanced == true || LaunchBar.options.commandKey) {
         var output = advancedOptions();
         return output;
       } else {
