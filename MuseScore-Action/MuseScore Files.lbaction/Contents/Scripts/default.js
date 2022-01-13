3379270ba8082ea4c0e57bc6d8378628fcaa5fea
Ptujec <derbenderchristian@gmail.com>
2022-01-13 11:59:42 +0100

    New

---
+++ MuseScore-Action/MuseScore Files.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,88 @@
+// LaunchBar Action Script
+var folderPath = Action.preferences.folderPath;
+
+function run() {
+  if (LaunchBar.options.shiftKey) {
+    var output = settings();
+    return output;
+  } else {
+    try {
+      var plist = File.readPlist(
+        '~/Library/Preferences/org.musescore.MuseScore3.plist'
+      );
+    } catch (exception) {
+      LaunchBar.alert('Error while reading plist: ' + exception);
+    }
+    if (Action.preferences.mode == 'folder') {
+      if (folderPath == undefined || folderPath == '') {
+        var folderPath = plist['application.paths.myScores'];
+
+        Action.preferences.folderPath = folderPath;
+      }
+
+      // var contents = File.getDirectoryContents(folderPath);
+      var contents = LaunchBar.execute('/bin/ls', '-tA', folderPath)
+        .trim()
+        .split('\n');
+
+      var results = [];
+      contents.forEach(function (item) {
+        var path = folderPath + '/' + item;
+
+        if (!item.startsWith('.')) {
+          results.push({
+            title: item,
+            // subtitle: '',
+            path: path,
+          });
+        }
+      });
+
+      return results;
+    } else {
+      var results = [];
+
+      for (var i = 0; i < 19; i++) {
+        var recentPath = plist['recent-' + i];
+        if (!recentPath.startsWith(':')) {
+          results.push({
+            path: recentPath,
+          });
+        }
+      }
+      return results;
+    }
+  }
+}
+
+function settings() {
+  if (Action.preferences.mode == 'folder') {
+    var folderIcon = 'folderSelectedTemplate';
+    var recentIcon = 'recentTemplate';
+  } else {
+    var folderIcon = 'folderTemplate';
+    var recentIcon = 'recentSelectedTemplate';
+  }
+
+  return [
+    {
+      title: 'Scores Folder',
+      icon: folderIcon,
+      action: 'setMode',
+      actionArgument: 'folder',
+    },
+    {
+      title: 'Recent Files',
+      icon: recentIcon,
+      action: 'setMode',
+      actionArgument: 'recent',
+    },
+  ];
+}
+
+function setMode(mode) {
+  Action.preferences.mode = mode;
+
+  var output = settings();
+  return output;
+}
---
+++ MuseScore-Action/README.md
@@ -0,0 +1,16 @@
+# LaunchBar Action: MuseScore Files
+
+Missing recent files, when you select MuseScore in LaunchBar and press space? 
+
+This action fixes that. It is associated with MuseScore 3, so it works like it does with most native apps that handle files (e.g. Pages, Keynote, …). 
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/lLZgKIthbOk?start=82" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+## Settings
+If you prefer to show your whole scores folder (sorted by last used) you can set this up in settings  (⇧⏎). 
+
+Note: You need to select the actual action to get to the settings! 
+
+## Download
+
+[Download LaunchBar Action: MuseScore Files](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/MuseScore-Action) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
