d5706da822c850c9ba74e09f78fa9a56a9246764
Ptujec <derbenderchristian@gmail.com>
2022-01-25 20:02:14 +0100

    1.1
    Using a different select URI pattern (same as Zutilo plugin)

--- Zotero-Actions/Show Attachment in Zotero.lbaction/Contents/Scripts/default.js
+++ Zotero-Actions/Show Attachment in Zotero.lbaction/Contents/Scripts/default.js
@@ -1,9 +1,8 @@
 /* Show PDF in Zotero
- 
-*/
+ */
 
 function run(path) {
   var itemKey = path.toString().match(/Zotero\/storage\/(.*)\//)[1];
-  var zURL = 'zotero://select/items/0_' + itemKey;
+  var zURL = 'zotero://select/library/items/' + itemKey;
   LaunchBar.openURL(zURL);
 }
