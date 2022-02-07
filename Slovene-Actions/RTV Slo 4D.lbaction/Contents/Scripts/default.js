376afc54f2b05e6ca7480a7c923896cf8b462de4
Ptujec <derbenderchristian@gmail.com>
2022-02-07 01:26:45 +0100

    1.1
    - display clip duration
    - show up to 20 hits (before 12)

--- Slovene-Actions/RTV Slo 4D.lbaction/Contents/Scripts/default.js
+++ Slovene-Actions/RTV Slo 4D.lbaction/Contents/Scripts/default.js
@@ -2,15 +2,16 @@ function run(argument) {
   var recordings = HTTP.getJSON(
     'https://api.rtvslo.si/ava/getSearch2?client_id=82013fb3a531d5414f478747c1aca622&q=' +
       encodeURI(argument) +
-      '&showTypeId=&sort=date&order=desc&pageSize=12&pageNumber=0&subtitled=&clip=clip&from=2007-01-01'
+      '&showTypeId=&sort=date&order=desc&pageSize=20&pageNumber=0&subtitled=&clip=clip&from=2007-01-01'
   ).data.response.recordings;
 
   var result = [];
   recordings.forEach(function (item) {
     var rDate = item.broadcastDate;
     var showName = item.showName;
+    var duration = item['length'];
 
-    var sub = rDate + ', ' + showName;
+    var sub = rDate + ', ' + showName + ' (' + duration + ')';
 
     result.push({
       title: item.title,
