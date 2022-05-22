aa19bd803057fd03a24ec02da7695157a2732fdb
Ptujec <derbenderchristian@gmail.com>
2022-05-22 17:06:55 +0200

    1.0

---
+++ SF-Symbols-Browser/README.md
@@ -0,0 +1,18 @@
+# LaunchBar Action: SF-Symbols Browser 
+
+If you know what SF-Symbols are you probably have the [app installed](https://developer.apple.com/sf-symbols/) along with the [fonts](https://developer.apple.com/fonts/).
+
+This action enables you to browse through them without opening the app. If you have the app installed you can simply select it and press space  `␣`.
+
+<img src="sfs.jpg" width="706"/> 
+
+You can than either paste the symbol `↩` or the name `⌥↩`.
+
+
+## Download
+
+[Download LaunchBar Action: SF-Symbols Browser](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/SF-Symbols-Browser) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
+
+## Updates
+
+This action integrates with Action Updates by @prenagha. You can find the [latest version in his Github repository](https://github.com/prenagha/launchbar). For more information and a signed version of Action Updates [visit his website](https://renaghan.com/launchbar/action-updates/).
---
+++ SF-Symbols-Browser/SF-Symbols Browser.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,106 @@
+/* 
+SF Symbols Browser Action for LaunchBar
+by Christian Bender (@ptujec)
+2022-05-22
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+
+- https://stackoverflow.com/questions/6484670/how-do-i-split-a-string-into-an-array-of-characters
+*/
+
+const sfSymbolsLocation = Action.supportPath + '/sfSymbols.json';
+const recentSymbolsLocation = Action.supportPath + '/recentSymbols.json';
+const infoPlistLocation = Action.path + '/Contents/Info.plist';
+
+function run(argument) {
+  var actionVersion = File.readPlist(infoPlistLocation).CFBundleVersion;
+
+  // Check First Run
+  if (Action.preferences.firstRun != false) {
+    Action.preferences.firstRun = false;
+    var createJSON = true;
+    Action.preferences.firstRunActionVersion = actionVersion;
+  } else {
+    // Check if Version Number is the same
+    if (actionVersion == Action.preferences.firstRunActionVersion) {
+      var createJSON = false;
+    } else {
+      var createJSON = true;
+      Action.preferences.firstRunActionVersion = actionVersion;
+    }
+  }
+
+  // Create JSON
+  if (createJSON == true) {
+    var symbols = File.readText(
+      Action.path + '/Contents/Resources/sfsymbols.txt'
+    ).split(/(?!$)/u);
+
+    var text = File.readText(
+      Action.path + '/Contents/Resources/sfsymbols_names.txt'
+    ).split('\n');
+
+    var result = [];
+    for (var i = 0; i < text.length; i++) {
+      var title = text[i];
+
+      var pushData = {
+        title: title,
+        icon: 'character:' + symbols[i],
+        index: i,
+        action: 'action',
+        actionArgument: {
+          symbol: symbols[i],
+          title: title,
+        },
+      };
+
+      result.push(pushData);
+    }
+    File.writeJSON(result, sfSymbolsLocation);
+  }
+
+  var sfSymbols = File.readJSON(sfSymbolsLocation);
+
+  sfSymbols.sort(function (a, b) {
+    // return a.usage < b.usage;
+    return b.index < a.index;
+  });
+
+  if (File.exists(recentSymbolsLocation)) {
+    var recentSymbols = File.readJSON(recentSymbolsLocation);
+    sfSymbols = recentSymbols.reverse().concat(sfSymbols);
+  }
+  return sfSymbols;
+}
+
+function action(dict) {
+  if (File.exists(recentSymbolsLocation)) {
+    var recentSymbols = File.readJSON(recentSymbolsLocation);
+  } else {
+    var recentSymbols = [];
+  }
+
+  recentSymbols.push({
+    title: dict.title,
+    icon: 'character:' + dict.symbol,
+    action: 'action',
+    badge: 'recent',
+    actionArgument: {
+      symbol: dict.symbol,
+      title: dict.title,
+    },
+  });
+
+  if (recentSymbols.length > 3) {
+    recentSymbols.splice(0, 1);
+  }
+
+  File.writeJSON(recentSymbols, recentSymbolsLocation);
+
+  if (LaunchBar.options.alternateKey) {
+    LaunchBar.paste(dict.title);
+  } else {
+    LaunchBar.paste(dict.symbol);
+  }
+}
