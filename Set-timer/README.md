17313f04bdf39ba9bcacb2a96b514abc761cc5b9
Ptujec <derbenderchristian@gmail.com>
2022-11-06 13:49:19 +0100

    1.0
    This action makes use of the new built-in clock app. It let's you set timers.

---
+++ Set-timer/README.md
@@ -0,0 +1,26 @@
+# LaunchBar Action: Set timer
+
+<img src="01.jpg" width="641"/> 
+
+This action makes use of the new built-in clock app. It let's you set timers.
+
+Examples: 
+- "1m" or just "1" = 1 minute
+- "30s" = 30 seconds
+- "1h" = 1 hour
+
+It relies on a shortcut so it needs a tiny bit of set up.
+
+## Set up
+
+1) Install the shortcut named ["Set timer"](https://www.icloud.com/shortcuts/6769921fa76d437faa22bec595020ede)
+
+2) Download and install the action as usual below. 
+
+## Download
+
+[Download LaunchBar Action: Set timer](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Set-timer) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
+
+## Updates
+
+This action integrates with Action Updates by @prenagha. You can find the [latest version in his Github repository](https://github.com/prenagha/launchbar). For more information and a signed version of Action Updates [visit his website](https://renaghan.com/launchbar/action-updates/).
---
+++ Set-timer/Set timer.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,54 @@
+/* 
+Set timer Action for LaunchBar
+by Christian Bender (@ptujec)
+2022-11-06
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
+
+function run(argument) {
+  var list = LaunchBar.execute('/usr/bin/shortcuts', 'list').trim().split('\n');
+
+  if (!list.includes('Set timer')) {
+    var response = LaunchBar.alert(
+      'Shortcut missing!',
+      'The action needs the shortcut named "Set alarm" to run properly. Do you want to install it now?',
+      'Yes',
+      'Cancel'
+    );
+    switch (response) {
+      case 0:
+        LaunchBar.openURL(
+          'https://www.icloud.com/shortcuts/6769921fa76d437faa22bec595020ede'
+        );
+
+      case 1:
+        break;
+    }
+  } else {
+    LaunchBar.hide();
+    if (!isNaN(argument)) {
+      // default to minutes
+      argument = argument + 'm';
+    }
+
+    var m = argument.match(/(\d+)([msh])?/i);
+
+    var amount = m[1];
+    var unit = m[2];
+
+    if (unit == 'h') {
+      var seconds = amount * 3600;
+    } else if (unit == 'm') {
+      var seconds = amount * 60;
+    } else {
+      var seconds = amount;
+    }
+
+    LaunchBar.executeAppleScript(
+      'tell application "Shortcuts Events" to run shortcut "Set timer" with input "' +
+        seconds +
+        '"'
+    );
+  }
+}
