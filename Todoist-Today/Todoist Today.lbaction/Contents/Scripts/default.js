d6e793650f52f219266a6d3ae2ed0f2d47b1da16
Ptujec <derbenderchristian@gmail.com>
2022-12-30 16:44:57 +0100

    2.4.1
    Update Set API function

--- Todoist-Today/Todoist Today.lbaction/Contents/Scripts/default.js
+++ Todoist-Today/Todoist Today.lbaction/Contents/Scripts/default.js
@@ -108,22 +108,51 @@ function run(argument) {
 function setApiKey() {
   var response = LaunchBar.alert(
     'API-Token required',
-    '1) Go to Settings/Integrations and copy the API-Token.\n2) Press »Set API-Token«',
+    '1) Go to Settings/Integrations/Developer and copy the API-Token.\n2) Press »Set API-Token«',
     'Open Settings',
     'Set API-Token',
     'Cancel'
   );
   switch (response) {
     case 0:
-      LaunchBar.openURL('https://todoist.com/app/settings/integrations');
+      LaunchBar.openURL(
+        'https://todoist.com/app/settings/integrations/developer'
+      );
       LaunchBar.hide();
       break;
     case 1:
-      Action.preferences.apiToken = LaunchBar.getClipboardString().trim();
-      LaunchBar.alert(
-        'Success!',
-        'API-Token set to: ' + Action.preferences.apiToken
-      );
+      var clipboardContent = LaunchBar.getClipboardString().trim();
+
+      if (clipboardContent.length == 40) {
+        // Test API-Token
+        var projectsOnline = HTTP.getJSON(
+          'https://api.todoist.com/rest/v2/projects',
+          {
+            headerFields: {
+              Authorization: 'Bearer ' + clipboardContent,
+            },
+          }
+        );
+
+        if (projectsOnline.error != undefined) {
+          LaunchBar.alert(projectsOnline.error);
+        } else {
+          // Write new API-Token in Action preferences
+          Action.preferences.apiToken = clipboardContent;
+
+          LaunchBar.alert(
+            'Success!',
+            'API-Token set to: ' +
+              Action.preferences.apiToken +
+              '.\nProjects, sections and labels will be updated next.'
+          );
+        }
+      } else {
+        LaunchBar.alert(
+          'The length of the clipboard content does not match the length of a correct API-Token',
+          'Make sure the API-Token is the most recent item in the clipboard!'
+        );
+      }
       break;
     case 2:
       break;
