231dc2e977fa0fe7d25660c1f6c45864fb233fed
Ptujec <derbenderchristian@gmail.com>
2023-02-24 22:32:16 +0100

    4.4.4
    Improved abbreviation and uncommon book name recognition

--- Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Look up scripture.lbaction/Contents/Scripts/default.js
@@ -38,9 +38,8 @@ function run(argument) {
       .replace(/\(|\)/g, '')
       .replace(/(\s+)?([\-–,:])(\s+)?/g, '$2');
 
-    // Convert Slovene and German argument strings
     var mA = argument.match(
-      /(?:[1-5]\.?\s?)?(?:[a-zžščöäü]+\.?\s?)?[0-9,.:\-–f]+/gi
+      /(?:[1-5]\.?\s?)?(?:[a-zžščöäü ]+\.?\s?)?[0-9,.:\-–f]+/gi
     );
 
     if (mA == undefined) {
@@ -56,7 +55,7 @@ function run(argument) {
         }
 
         var mB = scrip.match(
-          /([1-5]\.?\s?)?([a-zžščöäü]+\.?\s?)?([0-9,.:\-–f]+)/i
+          /([1-5]\.?\s?)?([a-zžščöäü ]+\.?\s?)?([0-9,.:\-–f]+)/i
         );
 
         var prefix = mB[1];
@@ -72,24 +71,7 @@ function run(argument) {
         if (bookName == undefined) {
           bookName = '';
         } else {
-          bookName = bookName.trim().replace(/\./, '').toLowerCase();
-
-          // Replace alternative booknames and abbreviations with the english name (so Accordance can parse it correctly)
-          var newBookName = '';
-          bookNameDictionary.booknames.forEach(function (item) {
-            var altNames = item.alt;
-            for (var i = 0; i < altNames.length; i++) {
-              var name = altNames[i].toLowerCase();
-              if (name.startsWith(bookName)) {
-                newBookName = item.english;
-                break;
-              }
-            }
-          });
-
-          if (newBookName != '') {
-            bookName = newBookName + ' ';
-          }
+          bookName = replaceBookName(bookName);
         }
         var suffix = mB[3];
 
@@ -103,11 +85,11 @@ function run(argument) {
       .toString()
       .replace(/ ,/g, '; ')
       .trim()
-      .replace(/1 Moses|1Moses/, 'Genesis')
-      .replace(/2 Moses|2Moses/, 'Exodus')
-      .replace(/3 Moses|3Moses/, 'Leviticus')
-      .replace(/4 Moses|4Moses/, 'Numbers')
-      .replace(/5 Moses|5Moses/, 'Deuteronomy');
+      .replace(/1 ?Moses/, 'Genesis')
+      .replace(/2 ?Moses/, 'Exodus')
+      .replace(/3 ?Moses/, 'Leviticus')
+      .replace(/4 ?Moses/, 'Numbers')
+      .replace(/5 ?Moses/, 'Deuteronomy');
   }
   var output = lookUp(result, argument);
   return output;
@@ -280,4 +262,50 @@ function lookupInTranslation(dict) {
   LaunchBar.openURL(
     'accord://read/' + encodeURI(translation) + '?' + encodeURI(result)
   );
+}
+
+function replaceBookName(bookName) {
+  // Replace alternative booknames and abbreviations with the english name (so Accordance can parse it correctly)
+  var newBookName = '';
+  bookName = bookName.trim().replace(/\./, '').toLowerCase();
+
+  var bookNames = bookNameDictionary.booknames;
+
+  for (var i = 0; i < bookNames.length; i++) {
+    var englishName = bookNames[i].english.toLowerCase();
+    var altNames = bookNames[i].alt;
+
+    if (englishName.startsWith(bookName)) {
+      newBookName = bookNames[i].english;
+      break;
+    }
+
+    for (var j = 0; j < altNames.length; j++) {
+      var altName = altNames[j].toLowerCase();
+
+      if (altName.startsWith(bookName)) {
+        newBookName = bookNames[i].english;
+        var isBreak = true;
+        break;
+      }
+    }
+    var abbrs = bookNames[i].abbr;
+    for (var k = 0; k < abbrs.length; k++) {
+      var abbr = abbrs[k].toLowerCase();
+      if (bookName == abbr) {
+        newBookName = bookNames[i].english;
+        var isBreak = true;
+        break;
+      }
+    }
+
+    if (isBreak == true) {
+      break;
+    }
+  }
+
+  if (newBookName != '') {
+    bookName = newBookName + ' ';
+  }
+  return bookName;
 }
