5422ec765a2f91efb3e893022f24e4fb6600fd88
Ptujec <derbenderchristian@gmail.com>
2023-02-25 21:39:03 +0100

    1.0

---
+++ Search-Actions/Wikipedia EN.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,33 @@
+/* 
+Wikipedia (EN) Action for LaunchBar
+by Christian Bender (@ptujec)
+2023-02-25
+
+- https://en.wikipedia.org/api/rest_v1/#/Page%20content
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
+
+function run(argument) {
+  if (!LaunchBar.options.commandKey) {
+    LaunchBar.openURL(
+      'http://en.wikipedia.org/wiki/' + encodeURIComponent(argument)
+    );
+  } else {
+    var extract = HTTP.getJSON(
+      'https://en.wikipedia.org/api/rest_v1/page/summary/' +
+        encodeURIComponent(argument)
+    );
+
+    var result = extract.data.extract;
+
+    if (result != undefined) {
+      result = result.replace(/\n/g, ' ');
+      LaunchBar.performAction('Text vergrößern', result);
+    } else {
+      LaunchBar.openURL(
+        'http://en.wikipedia.org/wiki/' + encodeURIComponent(argument)
+      );
+    }
+  }
+}
---
+++ Search-Actions/Wikipedia EN.lbaction/Contents/Scripts/suggestions.js
@@ -0,0 +1,41 @@
+/* 
+Wikipedia (EN) Action for LaunchBar
+by Christian Bender (@ptujec)
+2023-02-25
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
+
+function runWithString(argument) {
+  if (argument == '') {
+    return;
+  }
+
+  var data = HTTP.getJSON(
+    'https://en.wikipedia.org/w/rest.php/v1/search/title?q=' +
+      encodeURI(argument) +
+      '&limit=20'
+  );
+
+  if (data.response.status != 200) {
+    return;
+  }
+
+  var suggestions = [];
+  var pages = data.data.pages;
+
+  pages.forEach(function (item) {
+    pushData = {
+      title: item.title,
+      icon: 'wikiconTemplate',
+    };
+
+    if (item.description != null) {
+      pushData.subtitle = item.description;
+    }
+
+    suggestions.push(pushData);
+  });
+
+  return suggestions;
+}
