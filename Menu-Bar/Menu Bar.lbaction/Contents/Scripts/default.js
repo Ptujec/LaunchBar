b43daa13cbe9f5356502d729487775f0a014f88b
Ptujec <derbenderchristian@gmail.com>
2023-03-05 13:13:26 +0100

    1.0

---
+++ Menu-Bar/Menu Bar.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,99 @@
+/* 
+Menu Bar Items (powered by Finbar) Action for LaunchBar
+by Christian Bender (@ptujec)
+2023-03-04
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
+
+const finbarCLI = '/Applications/Finbar.app/Contents/MacOS/finbar-cli';
+
+function run() {
+  var list = JSON.parse(LaunchBar.execute(finbarCLI, 'list'));
+  var recentItems = Action.preferences.recentItems;
+
+  var resultRecent = [];
+  var resultAll = [];
+
+  list.forEach(function (item) {
+    var sub = item.path.join(' > ');
+    var title = item.title;
+
+    if (item.mark != undefined) {
+      title = title + ' ' + item.mark;
+    }
+
+    if (
+      // This if statement is just for as long as there is not flag in the CLI to apply Finbar rules
+      item.indices[0] != 0 &&
+      !item.path.includes('Open Recent') &&
+      !item.path.includes('Benutzte Dokumente') &&
+      !item.path.includes('Verlauf') &&
+      !item.path.includes('History') &&
+      !item.path.includes('Services') &&
+      !item.path.includes('Dienste')
+    ) {
+      var pushData = {
+        title: title,
+        subtitle: sub,
+        icon: item.bundleID,
+        action: 'click',
+        actionArgument: item,
+        actionRunsInBackground: true,
+      };
+
+      if (item.shortcut != undefined) {
+        pushData.badge = item.shortcut;
+      }
+
+      // Priorize recent item
+      var recent = false;
+      var uID = item.bundleID + item.path.join(' ');
+
+      if (recentItems != undefined) {
+        recentItems.forEach(function (item) {
+          var recentUID = item.bundleID + item.path.join(' ');
+          if (recentUID == uID) {
+            recent = true;
+          }
+        });
+      }
+
+      if (recent == true) {
+        resultRecent.push(pushData);
+      } else {
+        resultAll.push(pushData);
+      }
+    }
+  });
+
+  var result = resultRecent.concat(resultAll);
+  return result;
+}
+
+function click(item) {
+  LaunchBar.hide();
+
+  /// Remember Used Menu Item (per App)
+  var recentItems = Action.preferences.recentItems;
+  if (recentItems == undefined) {
+    recentItems = [];
+  }
+
+  recentItems.forEach(function (item2, index) {
+    if (item2.bundleID == item.bundleID) {
+      recentItems.splice(index, 1);
+    }
+  });
+
+  recentItems.push(item);
+  Action.preferences.recentItems = recentItems;
+
+  // Click menu item
+  LaunchBar.execute(
+    finbarCLI,
+    'select',
+    ...item.indices,
+    `--title=${item.title}`
+  );
+}
---
+++ Menu-Bar/README.md
@@ -0,0 +1,23 @@
+# LaunchBar Action: Menu Bar (powered by Finbar)
+
+<img src="menubar.png" width="660"/> 
+
+This action allows to trigger menu bar items right from within LaunchBar. **It requires [Finbar](https://www.roeybiran.com/apps/finbar), a wonderful application by Roey Biran**. 
+
+The action will list all menu bar items of the frontmost application. You can then type to narrow down results. 
+
+The main benefit of this approach is that it utilizes the superpower of LaunchBar. You’ll find stuff more easily without knowing the exact name. And LaunchBar learns and remembers your preferences. 
+
+Let's say you want to export a PDF in Pages. You type `P` to find `PDF …` more easily, select the item and hit enter. The next time you type `P` the PDF export item will be on top of the list.
+
+In addition to that the action remembers the last used item per application. So if you want to trigger the same menu bar item again it's right on top of the list. 
+
+**Note**: Version 1.0 excludes some menu bar items like e.g. "Apple", "Open recent" to avoid clutter. In an updated version the action will respect the rules you set up in Finbar.  
+
+## Download
+
+[Download LaunchBar Action: Menu Bar](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Menu-Bar) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
+
+## Updates
+
+This action integrates with Action Updates by @prenagha. You can find the [latest version in his Github repository](https://github.com/prenagha/launchbar). For more information and a signed version of Action Updates [visit his website](https://renaghan.com/launchbar/action-updates/).
