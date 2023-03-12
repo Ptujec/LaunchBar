bb6c357a3db90980106b7cf8abedf47d26fe1cdd
Ptujec <derbenderchristian@gmail.com>
2023-03-12 23:52:51 +0100

    1.0

---
+++ Passwords/Passwords.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,205 @@
+/* 
+Passwords - 1Password 8 Action for LaunchBar
+by Christian Bender (@ptujec)
+2023-03-10
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+
+Documentation: 
+- https://developer.1password.com/docs/cli/create-item/#retrieve-an-item
+- https://developer.1password.com/docs/cli/reference/management-commands/item#item-get
+- https://github.com/dteare/opbookmarks#app-integration
+
+Alfed Workflow: 
+- https://github.com/alfredapp/1password-workflow
+Raycast Extension:
+- https://github.com/khasbilegt/1Password/tree/main
+
+TODO:
+- Missing Icons: 
+  - Database  
+  - Driver License                 
+  - Outdoor License              
+  - Server
+  - Social Security Number    
+- Detect if app is locked (waiting on info how to)
+- Settings
+  - require login?
+  - update on open item
+  - show/hide additional_information (subtitles)
+- Warnung: 
+  - lokal aber unverschlüsselte Daten (keine Passwörter)
+  - wenn locked get autofill nicht … erst beim zweiten mal danach
+  - autofill geht manchmal nicht
+  - some category icons missing 
+- JSON verschlüsseln?
+*/
+
+const localJSONFile = Action.supportPath + '/list.json';
+
+function run() {
+  var accountID = Action.preferences.accountID;
+
+  if (accountID == undefined || LaunchBar.options.controlKey) {
+    var response = LaunchBar.alert(
+      'First run info',
+      'This actions requires 1Password\'s CLI.\nPress "Open Guide" for how to install and enable it.\nOn first run your account ID will be stored in Action Preferences. You can reset your account ID any time with "⌃↩".\nFor performance reasons the output is stored in a JSON file in the action\'s support folder. Refresh with "⌥↩".\nBoth the Preferences.plist and the JSON file can be found here: ~/Library/Application Support/LaunchBar/Action Support/ptujec.LaunchBar.action.Passwords/.',
+      'Open Guide',
+      'Get Started',
+      'Cancel'
+    );
+    switch (response) {
+      case 0:
+        LaunchBar.openURL(
+          'https://developer.1password.com/docs/cli/get-started#install'
+        );
+        break;
+      case 1:
+        setAccoundID();
+        break;
+      case 2:
+        break;
+    }
+    return;
+  }
+
+  if (LaunchBar.options.alternateKey || !File.exists(localJSONFile)) {
+    list = JSON.parse(updateLocalData());
+    LaunchBar.executeAppleScript('tell application "LaunchBar" to activate');
+  } else {
+    var list = File.readJSON(localJSONFile);
+  }
+
+  var results = [];
+  list.forEach(function (item) {
+    // Choose icon per category
+    var category = item.category.toLowerCase();
+    var iconPath = Action.path + '/Contents/Resources/' + category + '.png';
+
+    var pushData = {
+      title: item.title,
+      subtitle: item.additional_information,
+      label: item.vault.name,
+      icon: category,
+      updated: item.updated_at,
+      action: 'actions',
+      actionArgument: {
+        id: item.id,
+        category: category,
+        vaultId: item.vault.id,
+      },
+      actionRunsInBackground: true,
+    };
+
+    if (item.urls != undefined) {
+      item.urls.forEach(function (item) {
+        if (item.primary == true) {
+          pushData.actionArgument.url = item.href;
+        }
+      });
+    }
+
+    if (!File.exists(iconPath)) {
+      pushData.icon = 'genericTemplate';
+      // pushData.subtitle = category;
+    }
+
+    results.push(pushData);
+  });
+
+  results.sort(function (a, b) {
+    return a.icon.localeCompare(b.icon) || a.title.localeCompare(b.title);
+  });
+
+  return results;
+}
+
+function actions(item) {
+  LaunchBar.hide();
+
+  if (item.url == undefined) {
+    viewItem(item);
+    return;
+  }
+
+  if (item.category == 'login') {
+    if (!LaunchBar.options.commandKey) {
+      openURL(item);
+    } else {
+      viewItem(item);
+    }
+  } else {
+    if (!LaunchBar.options.commandKey) {
+      viewItem(item);
+    } else {
+      openURL(item);
+    }
+  }
+  return;
+}
+
+function setAccoundID() {
+  LaunchBar.hide();
+  LaunchBar.execute('/usr/local/bin/op', 'signin');
+
+  var accountID = LaunchBar.execute(
+    '/usr/local/bin/op',
+    'whoami',
+    '--format=json'
+  );
+
+  accountID = JSON.parse(accountID).account_uuid;
+  Action.preferences.accountID = accountID;
+}
+
+function getRandomID() {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  let result = '';
+  for (var i = 0; i < 26; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function updateLocalData() {
+  var list = LaunchBar.execute(
+    '/usr/local/bin/op',
+    'item',
+    'list',
+    '--format=json'
+  );
+  if (list == '') {
+    return;
+  }
+  File.writeText(list, localJSONFile);
+  return list;
+}
+
+function viewItem(item) {
+  // TODO: Detec if 1Password is locked and only use this when locked
+  var fileURL = File.fileURLForPath('/Applications/1Password.app');
+  LaunchBar.openURL(fileURL);
+
+  var urlScheme =
+    'onepassword://view-item/i?a=' +
+    Action.preferences.accountID +
+    '&v=' +
+    item.vaultId +
+    '&i=' +
+    item.id;
+
+  LaunchBar.openURL(urlScheme);
+  // updateLocalData();
+}
+
+function openURL(item) {
+  var url = item.url;
+  if (!url.startsWith('http')) {
+    url = 'https://' + url;
+  }
+
+  url = url + '?' + getRandomID() + '=' + item.id;
+  LaunchBar.openURL(url);
+  // updateLocalData();
+}
---
+++ Passwords/README.md
@@ -0,0 +1,33 @@
+# LaunchBar Action: Passwords for 1Password 8
+
+After a little setup (see below) you can view a list of all your login and other items from 1Password 8 in LaunchBar. 
+
+You can view items also when 1Password is selected simply by pressing `space`. 
+
+<img src="01.png" width="552"/>
+
+If you hit enter on a selected **login item** it will by default **open the url** and try to **autofill** the relevant information securely in your default browser. If you want to view the item in 1Password instead hit `⌘↩`.
+
+Other items will open in 1Password by default. If there is an url connected to an item (that is not a login item) you can open that with `⌘↩`. 
+
+**Note:** The data received from the CLI does not contain any passwords or secure information. You can enter `op item list --format=json` in the Terminal to see what data is retrieved. For performance reasons the output is stored in a JSON file in the action's support folder, which can be found in `~/Library/Application Support/LaunchBar/Action Support/`.
+
+## Setup
+
+This action requires 1Password's CLI. [Learn how to install and set up 1Password CLI on their website!](https://developer.1password.com/docs/cli/get-started#install). 
+
+## Limitations
+
+1) The action does not copy any passwords. In theory it would be possible to copy passwords to the clipboard. But those would also appear in your clipboard history. You would need to exclude LaunchBar from the clipboard history to prevent that. But this in turn would effect every other built in or custom action. 
+
+2) Because this is just a standard user action there is no indexing going on in the background. To refresh data press `⌥↩`. (You need to do that with the action selected. I won't work if you have 1Password selected.)
+
+3) Autofill and view item in 1Password actions won't work if 1Password 8 is locked.  
+
+## Download
+
+[Download LaunchBar Action: Passwords](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Ptujec/LaunchBar/tree/master/Passwords) (powered by [DownGit](https://github.com/MinhasKamal/DownGit))
+
+## Updates
+
+This action integrates with Action Updates by @prenagha. You can find the [latest version in his Github repository](https://github.com/prenagha/launchbar). For more information and a signed version of Action Updates [visit his website](https://renaghan.com/launchbar/action-updates/).
