39953686c047950d7342f54e1e5217786891005c
Ptujec <derbenderchristian@gmail.com>
2023-04-02 23:30:47 +0200

    1.1
    - version number change
    - action updates support

--- Converter-Actions/Temperatur - Celsius ⇄ Fahrenheit.lbaction/Contents/Scripts/default.js
+++ Converter-Actions/Temperatur - Celsius ⇄ Fahrenheit.lbaction/Contents/Scripts/default.js
@@ -1,19 +1,19 @@
 // Calculation Celius into Fahrenheit and vise versa
 
 function run(argument) {
-    let fahrenheit = (argument * 9 / 5) + 32;
-    let celsius = (argument - 32) * 5 / 9;
+  let fahrenheit = (argument * 9) / 5 + 32;
+  let celsius = ((argument - 32) * 5) / 9;
 
-    return [
-        {
-            title: fahrenheit.toFixed(1).replace('.', ',') + ' °F',
-            subtitle: argument.replace('.', ',') + ' °C',
-            icon: "ThermoTemplate"
-        },
-        {
-            title: celsius.toFixed(1).replace('.', ',') + ' °C',
-            subtitle: argument.replace('.', ',') + ' °F',
-            icon: "ThermoTemplate"
-        },
-    ];
+  return [
+    {
+      title: fahrenheit.toFixed(1).replace('.', ',') + ' °F',
+      subtitle: argument.replace('.', ',') + ' °C',
+      icon: 'ThermoTemplate',
+    },
+    {
+      title: celsius.toFixed(1).replace('.', ',') + ' °C',
+      subtitle: argument.replace('.', ',') + ' °F',
+      icon: 'ThermoTemplate',
+    },
+  ];
 }
