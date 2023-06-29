e77a60cab7cdc439e16b1878f8b8efdf20fbb9c0
Ptujec <derbenderchristian@gmail.com>
2023-06-29 15:00:19 +0200

    3.3
    - fix error where European Vers Notation converstion would lead to wrong format for some books
    - enable entry of just the book name (European Vers Notation)
    - code refactoring and cleanup

--- Accordance-Actions/Accordance - Paste scripture.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Paste scripture.lbaction/Contents/Scripts/default.js
@@ -13,98 +13,82 @@ Sources:
 
 String.prototype.localizationTable = 'default';
 
+const AccordancePrefs = eval(
+  File.readText('~/Library/Preferences/Accordance Preferences/General.apref')
+)[0]; // For default translation & vers notation settings
+
 const bookNameDictionary = File.readJSON(
   Action.path + '/Contents/Resources/booknames.json'
 ); // Currently contains German and Slovene names. You could expand it with your language by adding the relevant names to the "alt" array.
 
-const AccordancePrefs = eval(
-  File.readText('~/Library/Preferences/Accordance Preferences/General.apref')
-)[0];
+const textModulesPath =
+  '~/Library/Application Support/Accordance/Modules/Texts/';
 
 function run(argument) {
-  var translation = Action.preferences.translation;
-
-  if (translation == undefined) {
-    var translation =
-      AccordancePrefs['com.oaktree.settings.general.defaultsearchtext'];
-
-    Action.preferences.translation = translation;
-  }
+  const translation =
+    Action.preferences.translation ||
+    AccordancePrefs['com.oaktree.settings.general.defaultsearchtext'];
 
   if (LaunchBar.options.shiftKey) {
-    var output = settings();
-    return output;
+    return settings();
+  }
+
+  // Check Vers Notation Setting (see checkbox in "Appearance" section of Accoradance Preferences)
+  const num =
+    AccordancePrefs['com.oaktree.settings.general.useeuropeanversenotation'];
+
+  let result;
+
+  if (num === 0) {
+    // Default Vers Notation
+    result = argument;
   } else {
-    // Check Vers Notation Setting (see checkbox in "Appearance" section of Accoradance Preferences)
-    var num =
-      AccordancePrefs['com.oaktree.settings.general.useeuropeanversenotation'];
+    // European Vers Notation
+    argument = argument
+      // clean up capture (e.g. brackets) and formart errors (e.g. spaces before or after verse numbers) in entry
+      .trim()
+      .replace(/\(|\)/g, '')
+      .replace(/(\s+)?([\-–,:])(\s+)?/g, '$2');
 
-    if (num == 0) {
-      // Default Vers Notation
-      var result = argument;
-    } else {
-      // European Vers Notation
-      argument = argument
-        .trim()
-        // clean up capture (e.g. brackets) and formart errors (e.g. spaces before or after verse numbers) in entry
-        .replace(/\(|\)/g, '')
-        .replace(/(\s+)?([\-–,:])(\s+)?/g, '$2');
+    let matchAll = argument.match(
+      /(?:[1-5]\.?\s?)?(?:[a-zžščöäüß ]+\.?\s?)(?:[0-9,.:\-–f]+)?/gi
+    );
 
-      var mA = argument.match(
-        /(?:[1-5]\.?\s?)?(?:[a-zžščöäü ]+\.?\s?)?[0-9,.:\-–f]+/gi
+    result = [];
+    for (scrip of matchAll) {
+      scrip = scrip.trim();
+
+      // makes sure non-european styles get converted
+      if (scrip.includes(':')) {
+        scrip = scrip.replace(/,/g, '.').replace(/:/g, ',');
+      }
+
+      let matchOne = scrip.match(
+        /([1-5]\.?\s?)?([a-zžščöäü ]+\.?\s?)([0-9,.:\-–f]+)?/i
       );
 
-      var result = [];
-      for (var i = 0; i < mA.length; i++) {
-        var scrip = mA[i].trim();
+      let prefix = matchOne[1] ? matchOne[1].replace(/\./, '') : '';
+      let bookName = matchOne[2] ? replaceBookName(matchOne[2]) : '';
+      var suffix = matchOne[3] ? matchOne[3] : '1';
 
-        // makes sure non-european styles get converted
-        if (scrip.includes(':')) {
-          scrip = scrip.replace(/,/g, '.').replace(/:/g, ',');
-        }
+      var newScrip = `${prefix} ${bookName} ${suffix}`;
 
-        var mB = scrip.match(
-          /([1-5]\.?\s?)?([a-zžščöäü ]+\.?\s?)?([0-9,.:\-–f]+)/i
-        );
-
-        var prefix = mB[1];
-
-        if (prefix == undefined) {
-          prefix = '';
-        } else {
-          prefix = prefix.replace(/\./, '');
-        }
-
-        var bookName = mB[2];
-
-        if (bookName == undefined) {
-          bookName = '';
-        } else {
-          bookName = replaceBookName(bookName);
-        }
-        var suffix = mB[3];
-
-        var newScrip = prefix + bookName + suffix;
-
-        result.push(newScrip + ' ');
-      }
-      result = result
-        .toString()
-        .replace(/ ,/g, '; ')
-        .trim()
-        .replace(/1 ?Moses/, 'Genesis')
-        .replace(/2 ?Moses/, 'Exodus')
-        .replace(/3 ?Moses/, 'Leviticus')
-        .replace(/4 ?Moses/, 'Numbers')
-        .replace(/5 ?Moses/, 'Deuteronomy');
+      result.push(newScrip);
     }
 
-    if (LaunchBar.options.commandKey) {
-      var output = listTranslations(result, argument);
-      return output;
-    } else {
-      pasteText(result, argument, translation);
-    }
+    result = result
+      .join('; ')
+      .replace(/1 ?Moses/, 'Genesis')
+      .replace(/2 ?Moses/, 'Exodus')
+      .replace(/3 ?Moses/, 'Leviticus')
+      .replace(/4 ?Moses/, 'Numbers')
+      .replace(/5 ?Moses/, 'Deuteronomy');
+  }
+
+  if (LaunchBar.options.commandKey) {
+    return listTranslations(result, argument);
+  } else {
+    pasteText(result, argument, translation);
   }
 }
 
@@ -126,35 +110,54 @@ function pasteText(result, argument, translation) {
   }
 
   // Cleanup quote (Ony works if you have checked "Split discontiguous verses" in Citation settings)
-  text = text.replace(/(\s+)?\r\r(\s+)?/g, ' […]\n');
+  text = text.replace(/(\s+)?\r\r(\s+)?/g, ' […] ');
 
   // Cleanup Bible Text Abbreviation for User Bibles and Bibles with Lemmata
-  translationName = translation.replace(/°|-LEM/g, '');
+  const translationName = translation.replace(/°|-LEM/g, '');
+  // Uppercase first character of query
   argument = argument.charAt(0).toUpperCase() + argument.slice(1);
 
-  if (Action.preferences.format == 'markdown') {
-    LaunchBar.paste(
-      '> ' + text + ' (' + argument + ' ' + translationName + ')'
-    );
-  } else if (Action.preferences.format == 'citation') {
-    LaunchBar.paste('„' + text + '“\n\n' + argument + ' ' + translationName);
+  let format = Action.preferences.format;
+  let defaultText = text + ' (' + argument + ' ' + translationName + ')';
+
+  if (format == 'markdown') {
+    LaunchBar.paste('> ' + defaultText);
+  } else if (format == 'citation') {
+    LaunchBar.paste('„' + text + '“ ' + argument + ' ' + translationName);
   } else {
-    LaunchBar.paste(text + ' (' + argument + ' ' + translationName + ')');
+    LaunchBar.paste(defaultText);
   }
 }
 
+function replaceBookName(bookName) {
+  // Replace alternative booknames and abbreviations with the english name (so Accordance can parse it correctly)
+  bookName = bookName.trim().replace(/\./, '').toLowerCase();
+
+  const foundBook = bookNameDictionary.booknames.find((book) => {
+    const englishName = book.english.toLowerCase();
+    const altNames = book.alt.map((name) => name.toLowerCase());
+    const abbrs = book.abbr.map((abbr) => abbr.toLowerCase());
+
+    return (
+      englishName.startsWith(bookName) ||
+      altNames.some((altName) => altName.startsWith(bookName)) ||
+      abbrs.some((abbr) => abbr == bookName)
+    );
+  });
+
+  if (foundBook) bookName = foundBook.english;
+
+  return bookName;
+}
+
 function settings() {
-  // TODO: localize
-  if (
+  const formatIcon =
     Action.preferences.format == undefined ||
     Action.preferences.format == 'plain'
-  ) {
-    var formatIcon = 'plainTemplate';
-  } else if (Action.preferences.format == 'citation') {
-    var formatIcon = 'citationTemplate';
-  } else {
-    var formatIcon = 'markdownTemplate';
-  }
+      ? 'plainTemplate'
+      : Action.preferences.format == 'citation'
+      ? 'citationTemplate'
+      : 'markdownTemplate';
 
   return [
     {
@@ -196,196 +199,80 @@ function listFormats() {
 
 function setFormat(format) {
   Action.preferences.format = format;
-  var output = settings();
-  return output;
+  return settings();
 }
 
 function listTranslations(result, argument) {
-  var translations = File.getDirectoryContents(
-    '~/Library/Application Support/Accordance/Modules/Texts'
-  );
+  const isCommandKeyPressed = LaunchBar.options.commandKey;
+  const translations = File.getDirectoryContents(textModulesPath);
 
-  var defaultTranslation = [];
-  var lastUsedTranslation = [];
-  var rest = [];
-  for (var i = 0; i < translations.length; i++) {
-    var translation = translations[i].split('.')[0];
+  let defaultTranslation = [];
+  let lastUsedTranslation = [];
+  let rest = [];
 
-    if (translations[i].split('.')[1] == 'atext') {
-      var plistPath =
-        '~/Library/Application Support/Accordance/Modules/Texts/' +
-        translation +
-        '.atext/Info.plist';
+  translations.map((translationFile) => {
+    const translation = translationFile.split('.')[0];
+    const extension = translationFile.split('.')[1];
 
+    let translationName;
+    if (extension == 'atext') {
+      let plistPath = `${textModulesPath}${translation}.atext/Info.plist`;
       if (!File.exists(plistPath)) {
-        plistPath =
-          '~/Library/Application Support/Accordance/Modules/Texts/' +
-          translation +
-          '.atext/ExtraInfo.plist';
+        plistPath = `${textModulesPath}${translation}.atext/ExtraInfo.plist`;
       }
 
-      var plist = File.readPlist(plistPath);
-      var translationName = plist['com.oaktree.module.humanreadablename'];
-      if (translationName == undefined) {
-        var translationName = plist['com.oaktree.module.fullmodulename'];
-        if (translationName == undefined) {
-          var translationName = translation.trim().replace('°', '');
-        }
-      }
+      const plist = File.readPlist(plistPath);
+      translationName =
+        plist['com.oaktree.module.humanreadablename'] ||
+        plist['com.oaktree.module.fullmodulename'] ||
+        translation.trim().replace('°', '');
     } else {
-      var translationName = translation.trim().replace('°', '');
+      translationName = translation.trim().replace('°', '');
     }
 
-    if (LaunchBar.options.commandKey) {
-      var pushContent = {
-        title: translationName,
-        subtitle: argument,
-        action: 'setTranslation',
-        actionArgument: {
-          result: result,
-          argument: argument,
-          translation: translation,
-        },
-      };
+    const pushContent = {
+      title: translationName,
+      action: isCommandKeyPressed ? 'setTranslation' : 'setDefaultTranslation',
+      actionArgument: {
+        result: result,
+        argument: argument,
+        translation: translation,
+      },
+      icon: 'bookTemplate',
+    };
 
-      var translationUsage = Action.preferences.translationUsage;
-      if (translationUsage != undefined) {
-        for (var j = 0; j < translationUsage.length; j++) {
-          if (translationUsage[j].translation == translation) {
-            pushContent.usage = translationUsage[j].usage;
-            break;
-          } else {
-            pushContent.usage = 0;
-          }
-        }
-      }
-    } else {
-      var pushContent = {
-        title: translationName,
-        action: 'setDefaultTranslation',
-        actionArgument: translation,
-      };
-    }
+    const isLastUsed = translation === Action.preferences.lastUsed;
+    const isDefault = translation === Action.preferences.translation;
 
-    if (
-      translation === Action.preferences.lastUsed &&
-      LaunchBar.options.commandKey
-    ) {
+    if (isLastUsed && isCommandKeyPressed) {
       pushContent.icon = 'bookTemplate';
+      pushContent.badge = 'recent';
       lastUsedTranslation.push(pushContent);
-    } else if (
-      translation === Action.preferences.translation &&
-      !LaunchBar.options.commandKey
-    ) {
+    } else if (isDefault && !isCommandKeyPressed) {
       pushContent.icon = 'selectedBookTemplate';
       defaultTranslation.push(pushContent);
-    } else if (
-      translation === Action.preferences.translation &&
-      LaunchBar.options.commandKey
-    ) {
-      pushContent.icon = 'selectedBookTemplate';
+    } else if (isDefault && isCommandKeyPressed) {
       rest.push(pushContent);
     } else {
       pushContent.icon = 'bookTemplate';
       rest.push(pushContent);
     }
-  }
-  rest.sort(function (a, b) {
-    return b.usage - a.usage || a.title.localeCompare(b.title);
   });
 
-  if (LaunchBar.options.commandKey) {
-    var result = lastUsedTranslation.concat(rest);
-  } else {
-    var result = lastUsedTranslation.concat(defaultTranslation.concat(rest));
-  }
+  rest.sort((a, b) => a.title.localeCompare(b.title));
+
+  result = isCommandKeyPressed
+    ? lastUsedTranslation.concat(rest)
+    : lastUsedTranslation.concat(defaultTranslation.concat(rest));
   return result;
 }
 
-function setDefaultTranslation(translation) {
+function setDefaultTranslation({ translation }) {
   Action.preferences.translation = translation;
-  var output = listTranslations();
-  return output;
+  return listTranslations();
 }
 
-function setTranslation(aA) {
-  result = aA.result;
-  argument = aA.argument;
-  translation = aA.translation;
-
-  // Write usage data
-  var translationUsage = Action.preferences.translationUsage;
-
-  if (translationUsage == undefined) {
-    Action.preferences.translationUsage = [
-      {
-        translation: translation,
-        usage: 1,
-      },
-    ];
-  } else {
-    for (var i = 0; i < translationUsage.length; i++) {
-      if (translationUsage[i].translation == translation) {
-        var usage = translationUsage[i].usage;
-        Action.preferences.translationUsage[i].usage = usage + 1;
-        var found = true;
-      }
-    }
-    if (found != true) {
-      Action.preferences.translationUsage.push({
-        translation: translation,
-        usage: 1,
-      });
-    }
-  }
-
+function setTranslation({ result, argument, translation }) {
   Action.preferences.lastUsed = translation;
-
   pasteText(result, argument, translation);
-}
-
-function replaceBookName(bookName) {
-  // Replace alternative booknames and abbreviations with the english name (so Accordance can parse it correctly)
-  var newBookName = '';
-  bookName = bookName.trim().replace(/\./, '').toLowerCase();
-
-  var bookNames = bookNameDictionary.booknames;
-
-  for (var i = 0; i < bookNames.length; i++) {
-    var englishName = bookNames[i].english.toLowerCase();
-    var altNames = bookNames[i].alt;
-
-    if (englishName.startsWith(bookName)) {
-      newBookName = bookNames[i].english;
-      break;
-    }
-
-    for (var j = 0; j < altNames.length; j++) {
-      var altName = altNames[j].toLowerCase();
-
-      if (altName.startsWith(bookName)) {
-        newBookName = bookNames[i].english;
-        var isBreak = true;
-        break;
-      }
-    }
-    var abbrs = bookNames[i].abbr;
-    for (var k = 0; k < abbrs.length; k++) {
-      var abbr = abbrs[k].toLowerCase();
-      if (bookName == abbr) {
-        newBookName = bookNames[i].english;
-        var isBreak = true;
-        break;
-      }
-    }
-
-    if (isBreak == true) {
-      break;
-    }
-  }
-
-  if (newBookName != '') {
-    bookName = newBookName + ' ';
-  }
-  return bookName;
 }
