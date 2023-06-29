c38f1cc046b4df78f472720dccff78cb2485bd55
Ptujec <derbenderchristian@gmail.com>
2023-06-29 17:58:48 +0200

    5.4
    Fixes
    - fix search in specific translation
    - now can properly uses options when search in a specific translation
    - code cleanup

--- Accordance-Actions/Accordance - Search words.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Search words.lbaction/Contents/Scripts/default.js
@@ -1,63 +1,66 @@
-/* 2021-07-25 @Ptujec 
+/* 
+Accordance Search words Action for LaunchBar
+by Christian Bender (@ptujec)
+2023-06-29
 
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+
+Documentation:
+- https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
 - http://www.accordancebible.com/Accordance-1043-Is-Automagical/
-- http://accordancefiles2.com/helpfiles/OSX12/Default.htm#topics/05_dd/using_links_common_tasks.htm#kanchor184 (See: Examples of Accordance-specific URLs)
-- https://developer.obdev.at/launchbar-developer-documentation/#/javascript-file
+- https://accordancefiles2.com/helpfiles/14-macOS/index.htm#t=mac_14%2Fcontent%2Ftopics%2F05_dd%2Fusing_links_common_tasks.htm&rhsearch=url&rhhlterm=url%20urls (See: Examples of Accordance-specific URLs)
+
+The Original Accordance Automation Script Library:
+- http://macbiblioblog.blogspot.com/2009/01/downloads.html
 */
+
+const textModulesPath =
+  '~/Library/Application Support/Accordance/Modules/Texts/';
 
 function run(argument) {
   // Mode (search or research)
   if (argument.includes('#') || LaunchBar.options.commandKey) {
     if (LaunchBar.options.commandKey) {
-      // Choose Translation
-      var output = chooseTranslation(argument);
-      return output;
+      return chooseTranslation(argument);
     } else {
       LaunchBar.openURL('accord://search/?' + encodeURI(argument));
     }
-  } else {
-    // UI language check
-    var aPlist = File.readPlist(
-      '~/Library/Preferences/com.OakTree.Accordance.plist'
-    );
-    var lang = aPlist.AppleLanguages;
-
-    if (lang != undefined) {
-      lang = lang.toString();
-    } else {
-      var gPlist = File.readPlist(
-        '/Library/Preferences/.GlobalPreferences.plist'
-      );
-      lang = gPlist.AppleLanguages.toString();
-    }
-
-    if (lang.startsWith('de')) {
-      var allSetting = '[Alle]';
-    } else {
-      var allSetting = '[All]';
-    }
-
-    var query = argument;
-    query = searchOptions(query);
-
-    // Fix for Slovene Unicode Characters (German Umlaute seem to be ok)
-    const substrings = ['č', 'ž', 'š', 'Č', 'Ž', 'Š'];
-    if (substrings.some((v) => query.includes(v))) {
-      LaunchBar.openURL(
-        'accord://research/' + allSetting + ';Unicode?' + encodeURI(query)
-      );
-    } else {
-      LaunchBar.openURL(
-        'accord://research/' + allSetting + ';?' + encodeURI(query)
-      );
-    }
+    return;
   }
+
+  let allSetting = getLanguage().startsWith('de') ? '[Alle]' : '[All]';
+
+  // Fix for Slovene Unicode Characters (German Umlaute seem to be ok)
+  const substrings = ['č', 'ž', 'š', 'Č', 'Ž', 'Š'];
+  const suffix = substrings.some((v) => argument.includes(v))
+    ? ';Unicode?'
+    : '?';
+
+  const query = searchOptions(argument);
+
+  LaunchBar.openURL(
+    'accord://research/' + allSetting + suffix + encodeURI(query)
+  );
 }
 
-function searchOptions(query) {
+function getLanguage() {
+  const accPlist = File.readPlist(
+    '~/Library/Preferences/com.OakTree.Accordance.plist'
+  );
+  let lang = accPlist.AppleLanguages;
+  if (!lang) {
+    const globalPlist = File.readPlist(
+      '/Library/Preferences/.GlobalPreferences.plist'
+    );
+    lang = globalPlist.AppleLanguages.toString();
+  }
+  return lang.toString();
+}
+
+function searchOptions(argument) {
   // Search options:
   // A = <AND>, O = <OR>, N = <NOT>
-  query = query
+  query = argument
     .trim()
     .replace(/(:)\s+/, '$1')
     .replace(/\s+(#)/, '$1')
@@ -73,111 +76,64 @@ function searchOptions(query) {
 }
 
 function chooseTranslation(argument) {
-  var translations = File.getDirectoryContents(
-    '~/Library/Application Support/Accordance/Modules/Texts'
-  );
+  const translations = File.getDirectoryContents(textModulesPath);
 
-  var lastUsedTranslation = [];
-  var otherTranslations = [];
-  for (var i = 0; i < translations.length; i++) {
-    var translation = translations[i].split('.')[0];
+  let lastUsedTranslation = [];
+  let rest = [];
 
-    if (translations[i].split('.')[1] == 'atext') {
-      var plistPath =
-        '~/Library/Application Support/Accordance/Modules/Texts/' +
-        translation +
-        '.atext/Info.plist';
+  translations.map((translationFile) => {
+    const translation = translationFile.split('.')[0];
+    const extension = translationFile.split('.')[1];
 
+    let translationName;
+    if (extension == 'atext') {
+      let plistPath = `${textModulesPath}${translation}.atext/Info.plist`;
       if (!File.exists(plistPath)) {
-        plistPath =
-          '~/Library/Application Support/Accordance/Modules/Texts/' +
-          translation +
-          '.atext/ExtraInfo.plist';
+        plistPath = `${textModulesPath}${translation}.atext/ExtraInfo.plist`;
       }
 
-      var plist = File.readPlist(plistPath);
-      var translationName = plist['com.oaktree.module.humanreadablename'];
-      if (translationName == undefined) {
-        var translationName = plist['com.oaktree.module.fullmodulename'];
-        if (translationName == undefined) {
-          var translationName = translation.trim().replace('°', '');
-        }
-      }
+      const plist = File.readPlist(plistPath);
+      translationName =
+        plist['com.oaktree.module.humanreadablename'] ||
+        plist['com.oaktree.module.fullmodulename'] ||
+        translation.trim().replace('°', '');
     } else {
-      var translationName = translation.trim().replace('°', '');
+      translationName = translation.trim().replace('°', '');
     }
 
-    var pushContent = {
+    const pushContent = {
       title: translationName,
       subtitle: argument,
-      icon: 'bookTemplate',
       action: 'searchInTranslation',
       actionArgument: {
         translation: translation,
         argument: argument,
       },
+      icon: 'bookTemplate',
     };
 
-    var translationUsage = Action.preferences.translationUsage;
+    const isLastUsed = translation === Action.preferences.lastUsed;
 
-    if (translationUsage != undefined) {
-      for (var j = 0; j < translationUsage.length; j++) {
-        if (translationUsage[j].translation == translation) {
-          pushContent.usage = translationUsage[j].usage;
-          break;
-        } else {
-          pushContent.usage = 0;
-        }
-      }
-    }
-
-    if (translation === Action.preferences.lastUsed) {
+    if (isLastUsed) {
+      pushContent.badge = 'recent';
       lastUsedTranslation.push(pushContent);
     } else {
-      otherTranslations.push(pushContent);
+      rest.push(pushContent);
     }
-  }
-  otherTranslations.sort(function (a, b) {
-    return b.usage - a.usage || a.title.localeCompare(b.title);
   });
 
-  var translationResult = lastUsedTranslation.concat(otherTranslations);
-  return translationResult;
+  rest.sort((a, b) => a.title.localeCompare(b.title));
+
+  return lastUsedTranslation.concat(rest);
 }
 
-function searchInTranslation(dict) {
-  var translation = dict.translation;
-  var argument = dict.argument;
-
-  // Write usage data
-  var translationUsage = Action.preferences.translationUsage;
-
-  if (translationUsage == undefined) {
-    Action.preferences.translationUsage = [
-      {
-        translation: translation,
-        usage: 1,
-      },
-    ];
-  } else {
-    for (var i = 0; i < translationUsage.length; i++) {
-      if (translationUsage[i].translation == translation) {
-        var usage = translationUsage[i].usage;
-        Action.preferences.translationUsage[i].usage = usage + 1;
-        var found = true;
-      }
-    }
-    if (found != true) {
-      Action.preferences.translationUsage.push({
-        translation: translation,
-        usage: 1,
-      });
-    }
-  }
-
+function searchInTranslation({ translation, argument }) {
   Action.preferences.lastUsed = translation;
+
+  let query = searchOptions(argument);
+
   LaunchBar.hide();
   LaunchBar.openURL(
-    'accord://search/' + encodeURI(translation) + '?' + encodeURI(argument)
+    'accord://search/' + encodeURI(translation) + ';Words?' + encodeURI(query)
   );
 }
