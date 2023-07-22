441b564c07f68f46455e07d86f83cc42684fe8f6
Ptujec <derbenderchristian@gmail.com>
2023-07-22 21:36:22 +0200

    Sopomenke 1.1
    code refactoring

--- Slovene-Actions/Sopomenke.lbaction/Contents/Scripts/default.js
+++ Slovene-Actions/Sopomenke.lbaction/Contents/Scripts/default.js
@@ -1,104 +1,81 @@
-// LaunchBar Action for Slovar sopomenk sodobne slovenščine (https://viri.cjvt.si/sopomenke/slv/)
+/* 
+Sopomenke Action for LaunchBar
+Slovar sopomenk sodobne slovenščine (https://viri.cjvt.si/sopomenke/slv/)
+by Christian Bender (@ptujec)
+2023-07-22
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
 
 function run(argument) {
-  if (argument != undefined) {
-    argument = argument.trim();
-    if (argument != '') {
-      var result = HTTP.getJSON(
-        'https://viri.cjvt.si/sopomenke/ajax_api/v1/slv/sinonimni/search/' +
-          encodeURIComponent(argument),
-        3
-      ).data;
+  argument = argument.trim();
 
-      if (result != undefined) {
-        var suggestions = [];
-        for (var i = 0; i < result.length; i++) {
-          var entry = result[i].headword;
-          var id = result[i].id;
-          var mainURL =
-            'https://viri.cjvt.si/sopomenke/slv/state?mw=' +
-            entry +
-            '&mid=' +
-            id +
-            '&source=main_page';
-          var dataURL =
-            'https://viri.cjvt.si/sopomenke/ajax_api/v1/slv/synonym/download/' +
-            id;
+  if (!argument) return;
 
-          suggestions.push({
-            title: entry,
-            icon: 'sTemplate',
-            action: 'getSynonyms',
-            actionArgument: {
-              entry: entry,
-              mainURL: mainURL,
-              dataURL: dataURL,
-            },
-          });
-        }
-      }
-      return suggestions;
-    }
-  } else {
-    // No argument passed, just open the website:
-    LaunchBar.hide();
-    LaunchBar.openURL('http://viri.cjvt.si/sopomenke/slv/');
-  }
+  const data = HTTP.getJSON(
+    `https://viri.cjvt.si/sopomenke/ajax_api/v1/slv/sinonimni/search/${encodeURIComponent(
+      argument
+    )}`,
+    3
+  ).data;
+
+  if (!data) return;
+
+  return data.map((item) => {
+    const headword = item.headword;
+    const id = item.id;
+    const mainURL = `https://viri.cjvt.si/sopomenke/slv/state?mw=${headword}&mid=${id}&source=main_page`;
+    const dataURL = `https://viri.cjvt.si/sopomenke/ajax_api/v1/slv/synonym/download/${id}`;
+
+    return {
+      title: headword,
+      icon: 'sTemplate',
+      action: 'getSynonyms',
+      actionArgument: {
+        headword,
+        mainURL,
+        dataURL,
+      },
+    };
+  });
 }
 
-function getSynonyms(dict) {
-  var entry = dict.entry;
-  var mainURL = dict.mainURL;
-  var dataURL = dict.dataURL;
-
-  var data = HTTP.loadRequest(dataURL, {
+function getSynonyms({ headword, mainURL, dataURL }) {
+  const data = HTTP.loadRequest(dataURL, {
     timeout: 5.0,
     method: 'GET',
     resultType: 'text',
   }).data.split('\n');
 
-  var result = [];
-  for (var i = 9; i < data.length - 1; i++) {
-    var columns = data[i].split('\t');
+  return data.slice(9, -1).map((row) => {
+    const columns = row.split('\t');
 
-    var synonym = columns[0];
-    var type = columns[1];
-    var frequency = columns[4];
+    const synonym = columns[0];
+    const type = columns[1];
+    const frequency = columns[4];
 
-    if (frequency != '' && frequency != '-') {
-      var label = type + ' (' + frequency + ')';
-    } else {
-      var label = type;
-    }
+    const label =
+      frequency && frequency != '-' ? `${type} (${frequency})` : type;
 
-    if (type == 'uporabniški') {
-      var label = undefined;
-      var icon = 'synonymUserTemplate';
-    } else {
-      var icon = 'synonymTemplate';
-    }
-
-    result.push({
+    return {
       title: synonym,
-      label: label,
-      badge: entry,
-      icon: icon,
+      label: type == 'uporabniški' ? '' : label,
+      badge: headword,
+      icon: type == 'uporabniški' ? 'synonymUserTemplate' : 'synonymTemplate',
       action: 'openURL',
       actionArgument: {
         title: synonym,
         url: mainURL,
       },
-    });
-  }
-
-  return result;
+    };
+  });
 }
 
-function openURL(dict) {
+function openURL({ title, url }) {
   if (LaunchBar.options.shiftKey) {
-    LaunchBar.paste(dict.title);
+    LaunchBar.paste(title);
   } else {
     LaunchBar.hide();
-    LaunchBar.openURL(encodeURI(dict.url));
+    LaunchBar.openURL(encodeURI(url));
   }
 }
