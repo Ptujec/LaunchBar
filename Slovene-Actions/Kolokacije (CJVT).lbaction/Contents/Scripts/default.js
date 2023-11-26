ebd77614a88fde040c53a2e6bc61cf9f931bb468
Ptujec <derbenderchristian@gmail.com>
2023-11-26 17:59:52 +0100

    2.0
    Due to website changes the old version doesn't work anymore. Unfortunately I had to simplify the new version quite a bit. But at least it works again.

--- Slovene-Actions/Kolokacije (CJVT).lbaction/Contents/Scripts/default.js
+++ Slovene-Actions/Kolokacije (CJVT).lbaction/Contents/Scripts/default.js
@@ -21,96 +21,9 @@ function run(argument) {
   if (!data) return;
 
   return data.map((item) => ({
-    title: item.candidate,
+    title: item.text,
+    badge: item.category,
     icon: 'kTemplate',
-    action: 'getRelatedWords',
-    actionArgument: item.headword_id.toString(),
+    url: 'https://viri.cjvt.si/kolokacije/slv/headword/' + item.id.toString(),
   }));
-}
-
-function getRelatedWords(headwordID) {
-  let data = HTTP.loadRequest(
-    'https://viri.cjvt.si/kolokacije/slv/subfragments/collocations/' +
-      headwordID,
-    {
-      timeout: 5.0,
-      method: 'GET',
-      resultType: 'text',
-    }
-  ).data;
-
-  const structures = JSON.parse(JSON.parse(data).json).structures;
-
-  let result = [];
-
-  for (const structure of structures) {
-    const category = structure.category;
-    const mappingId = structure.mapping_id;
-    const collocations = structure.collocations;
-    const preposition = structure.preposition;
-
-    for (const collocation of collocations) {
-      var form = collocation.form.replace(/<(\/)?.>/g, '');
-      var formId = collocation.id;
-
-      const pushData = {
-        title: form,
-        badge: preposition ? `${category} + ${preposition}` : category,
-        icon: 'connectionTemplate',
-        action: 'showExamples',
-        actionArgument: {
-          formId: formId.toString(),
-          url: `https://viri.cjvt.si/kolokacije/slv/headword/${headwordID}/structure/${mappingId}?example=${formId}#`,
-        },
-        frequency: collocation.frequency,
-        preposition: preposition ? preposition : '',
-      };
-
-      result.push(pushData);
-    }
-  }
-
-  return result.sort(
-    (a, b) => a.badge.localeCompare(b.badge) || b.frequency - a.frequency
-  );
-}
-
-function showExamples({ formId, url }) {
-  if (LaunchBar.options.commandKey) {
-    LaunchBar.openURL(url);
-    return;
-  }
-
-  let data = HTTP.loadRequest(
-    `https://viri.cjvt.si/kolokacije/slv/subfragments/examples/${formId}`,
-    {
-      timeout: 5.0,
-      method: 'GET',
-      resultType: 'text',
-    }
-  ).data;
-
-  const examples = JSON.parse(JSON.parse(data).json).examples;
-
-  let result = [];
-
-  for (const example of examples) {
-    const source = example.source;
-    const year = example.year;
-    const form = example.form;
-
-    const shortForm = form.match(/<.*>/)[0].replace(/<(\/)?.>/g, '');
-    const fullForm = form.replace(/<.>(.*?)<\/.>/g, (_, match) =>
-      match.toUpperCase()
-    );
-
-    result.push({
-      title: shortForm,
-      subtitle: `${fullForm} (${source}, ${year})`,
-      alwaysShowsSubtitle: true,
-      icon: 'resultTemplate',
-      children: [{ title: fullForm }],
-    });
-  }
-  return result;
 }
