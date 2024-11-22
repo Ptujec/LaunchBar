a5b20f50cec57e5d7be67a1e0eb0c39a56d84488
Ptujec <derbenderchristian@gmail.com>
2024-11-22 14:11:51 +0100

    Recent MindNode Next Documents 1.1.1
    - Implemented changes regarding how the lastModifiedDate is determined, as suggested by @hannesoid.
    https://github.com/Ptujec/LaunchBar/commit/1535243f285a48be0f1be70869db68f93f1699e8#r149401898
    - Also removed sorting by date in the JS part, as it is not needed.

--- Recent-MindNodeNext-Documents/Recent MindNode Next Documents.lbaction/Contents/Scripts/data.sh
+++ Recent-MindNodeNext-Documents/Recent MindNode Next Documents.lbaction/Contents/Scripts/data.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
-documents=$(sqlite3 -json "$1" "SELECT -- last modified dates consider the document (metadata) and all contributing operations' modification dates
-    d.documentID,
-    d.title,
-    MAX(
-        COALESCE(d.localModificationDate, '0001-01-01'),
-        COALESCE(d.serverModificationDate, '0001-01-01'),
-        COALESCE(MAX(o.localModificationDate), '0001-01-01'),
-        COALESCE(MAX(o.serverModificationDate), '0001-01-01')
-    ) AS lastModifiedDate
-FROM 
-    document d
-LEFT JOIN 
-    operation o
-ON 
-    d.documentID = o.documentID
-WHERE
-    d.trashDate IS NULL AND d.localDeletionDate IS NULL -- not trashed, and not marked for synced deletion
-GROUP BY 
-    d.documentID
-ORDER BY
-    lastModifiedDate DESC")
+documents=$(sqlite3 -json "$1" "
+    SELECT 
+        d.documentID,
+        d.title,
+        CASE 
+            WHEN COALESCE(MAX(o.localModificationDate), MAX(o.serverModificationDate)) IS NULL 
+            THEN COALESCE(d.localModificationDate, d.serverModificationDate)
+            ELSE COALESCE(MAX(o.localModificationDate), MAX(o.serverModificationDate))
+        END AS lastModifiedDate
+    FROM 
+        document d
+    LEFT JOIN 
+        operation o ON d.documentID = o.documentID
+    WHERE
+        d.trashDate IS NULL AND d.localDeletionDate IS NULL -- not trashed, and not marked for synced deletion
+    GROUP BY 
+        d.documentID
+    ORDER BY
+        lastModifiedDate DESC")
+
 if [ -z "${documents}" ]; then
-  documents="[]"
+    documents="[]"
 fi
 
 # assets=$(stat -f "%Sm %N" "$2"*)
 assets=$(ls -A "$2")
 echo "${documents}////${assets}"
-
-
--- Recent-MindNodeNext-Documents/Recent MindNode Next Documents.lbaction/Contents/Scripts/default.js
+++ Recent-MindNodeNext-Documents/Recent MindNode Next Documents.lbaction/Contents/Scripts/default.js
@@ -17,31 +17,28 @@ function run() {
     dataBaseDir,
     assetsDir
   );
+
   if (!output) return { title: 'Nothing found', icon: 'alert' };
 
   const [documentsJsonString, assetsInfo] = output.split('////');
   const assetsInfoArray = assetsInfo ? assetsInfo.split('\n') : [];
-
   const data = JSON.parse(documentsJsonString);
 
-  return data
-    .map((item) => {
-      const previewImage = assetsInfoArray.find((assetsInfoItem) =>
-        assetsInfoItem.startsWith(`${item.documentID}_full_`)
-      );
+  return data.map((item) => {
+    const previewImage = assetsInfoArray.find((assetsInfoItem) =>
+      assetsInfoItem.startsWith(`${item.documentID}_full_`)
+    );
 
-      return {
-        title: item.title,
-        icon: 'com.ideasoncanvas.mindnode',
-        // icon: path,
-        path: assetsDir + previewImage,
-        action: 'open',
-        actionArgument: `https://mindnode.com/document/${item.documentID}#${item.title}`,
-        actionRunsInBackground: true,
-        date: item.lastModifiedDate,
-      };
-    })
-    .sort((a, b) => a.date < b.date);
+    return {
+      title: item.title,
+      icon: 'com.ideasoncanvas.mindnode',
+      // icon: path,
+      path: assetsDir + previewImage,
+      action: 'open',
+      actionArgument: `https://mindnode.com/document/${item.documentID}#${item.title}`,
+      actionRunsInBackground: true,
+    };
+  });
 }
 
 function open(url) {
