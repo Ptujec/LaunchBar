9931b0736723f2536f05acac0bfef4aca559a2df
Ptujec <derbenderchristian@gmail.com>
2024-12-03 14:12:16 +0100

    Verbs 1.2.1
    - regex fix
    - some refactoring

--- Verbs-Action/Verbs.lbaction/Contents/Scripts/default.js
+++ Verbs-Action/Verbs.lbaction/Contents/Scripts/default.js
@@ -1,130 +1,94 @@
-/* LaunchBar Action: Verbs
-- List of irregular verbs for testing: https://conjugator.reverso.net/conjugation-irregular-verbs-english.html
- */
+/* 
+Verbs Action for LaunchBar
+by Christian Bender (@ptujec)
+2024-12-03
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
 
 function run(argument) {
-  var searchURL =
-    'https://www.theconjugator.com/english/verb/to+' +
-    encodeURI(argument) +
-    '.html';
+  const searchURL = `https://www.theconjugator.com/english/verb/to+${encodeURI(
+    argument
+  )}.html`;
 
-  var html = HTTP.loadRequest(searchURL, {
+  const html = HTTP.loadRequest(searchURL, {
     timeout: 5.0,
     method: 'GET',
     resultType: 'text',
   }).data;
 
-  // Check if exists
-  var doesNotExist = / does not exist./.test(html);
-
-  if (doesNotExist == true) {
-    var response = LaunchBar.alert(
+  if (/ does not exist./.test(html)) {
+    let response = LaunchBar.alert(
       'No hit!',
-      'Try on https://dictionary.cambridge.org?',
+      'Try on dictionary.cambridge.org?',
       'Open',
       'Cancel'
     );
-    switch (response) {
-      case 0:
-        LaunchBar.openURL(
-          'https://dictionary.cambridge.org/dictionary/english/' + argument
-        );
-      case 1:
-        break;
+    if (response === 0) {
+      LaunchBar.openURL(
+        `https://dictionary.cambridge.org/dictionary/english/${argument}`
+      );
     }
-  } else {
-    // Check if what was entered is not the infinitive form
-    var reverse = /<h2 id="inv">Reverse conjugation/.test(html);
-    if (reverse == true) {
-      var newSearchURL =
-        'https://www.theconjugator.com/english/verb/' +
-        html.match(/<td><a href="\/english\/verb\/(.*?)">/)[1];
+    return [];
+  }
+  // File.writeText(html, Action.supportPath + '/test.html');
 
-      var html = HTTP.loadRequest(newSearchURL, {
-        timeout: 5.0,
-        method: 'GET',
-        resultType: 'text',
-      }).data;
-    }
+  if (/<h2 id="inv">Reverse conjugation/.test(html)) {
+    html = HTTP.loadRequest(
+      `https://www.theconjugator.com/english/verb/${
+        html.match(/<td><a href="\/english\/verb\/(.*?)">/)[1]
+      }`,
+      { timeout: 5.0, method: 'GET', resultType: 'text' }
+    ).data;
+  }
 
-    var verbs = html
-      .match(/<meta name="description" content="(.*?)" ?\/>/)[1]
-      .split(':')[1]
-      .replace(/\.$/, '')
-      .trim();
+  const verbs = html
+    .match(/<meta name="description" content="([^"]+)"/)[1]
+    .split(':')[1]
+    .replace(/\.$/, '')
+    .trim()
+    .split('-')
+    .map((v) => v.trim());
 
-    var detailsURLBase = 'https://dictionary.cambridge.org/dictionary/english/';
+  const [infinitive, past, participle] = verbs;
+  const detailsURLBase = 'https://dictionary.cambridge.org/dictionary/english/';
 
-    var result = [];
-
-    // Infinitive
-    var infinitive = verbs.split('-')[0].trim();
-    result.push({
+  return [
+    {
       title: infinitive,
       badge: 'infinitive',
       icon: 'oneTemplate',
       action: 'doStuff',
       actionArgument: {
         title: infinitive,
-        url: detailsURLBase + infinitive,
+        url: `${detailsURLBase}${infinitive}`,
       },
-    });
-
-    // Past Simple
-    var past = verbs.split('-')[1].trim();
-
-    var pastPushData = {
+    },
+    {
       title: past,
       badge: 'past (preterite)',
       icon: 'twoTemplate',
       action: 'doStuff',
       actionArgument: {
         title: past,
+        url: `${detailsURLBase}${past.split(', ')[0] || past}`,
       },
-    };
-
-    var wordCountPast = past.split(', ').length;
-
-    if (wordCountPast > 1) {
-      pastPushData.actionArgument.url = detailsURLBase + past.split(', ')[0];
-    } else {
-      pastPushData.actionArgument.url = detailsURLBase + past;
-    }
-
-    result.push(pastPushData);
-
-    // Past Participle
-    var participle = verbs.split('-')[2].trim();
-
-    var parPushData = {
+    },
+    {
       title: participle,
       badge: 'past participle',
       icon: 'threeTemplate',
       action: 'doStuff',
       actionArgument: {
         title: participle,
+        url: `${detailsURLBase}${participle.split(', ')[0] || participle}`,
       },
-    };
-
-    var wordCountParticiple = participle.split(', ').length;
-
-    if (wordCountParticiple > 1) {
-      parPushData.actionArgument.url =
-        detailsURLBase + participle.split(', ')[0];
-    } else {
-      parPushData.actionArgument.url = detailsURLBase + participle;
-    }
-
-    result.push(parPushData);
-
-    return result;
-  }
+    },
+  ];
 }
 
 function doStuff(dict) {
-  if (LaunchBar.options.shiftKey) {
-    LaunchBar.paste(dict.title);
-  } else {
-    LaunchBar.openURL(dict.url);
-  }
+  LaunchBar.options.shiftKey
+    ? LaunchBar.paste(dict.title)
+    : LaunchBar.openURL(dict.url);
 }
--- Verbs-Action/Verbs.lbaction/Contents/Scripts/suggestions.js
+++ Verbs-Action/Verbs.lbaction/Contents/Scripts/suggestions.js
@@ -1,30 +1,26 @@
-// LaunchBar Action Script
+/* 
+Verbs Action for LaunchBar
+by Christian Bender (@ptujec)
+2024-12-03
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
 
 function run(argument) {
   if (argument != '') {
-    var contents = HTTP.getJSON(
+    const contents = HTTP.getJSON(
       'https://www.the-conjugation.com/ajx/moteur.php?o=verbeAutoComplete&q=' +
         argument +
         '&l=en'
     ).data;
 
-    var suggestions = [];
-    contents.forEach(function (item) {
-      // item = item.replace(/^to /, '');
-      item = item.split('to ')[1];
-
-      if (item.startsWith(argument)) {
-        suggestions.push({
-          title: item,
-          icon: 'Template',
-        });
-      }
-    });
-
-    suggestions.sort(function (a, b) {
-      return a.title.length - b.title.length || a.title.localeCompare(b.title);
-    });
-
-    return suggestions;
+    return contents
+      .map((item) => item.split('to ')[1])
+      .filter((item) => item.startsWith(argument))
+      .map((item) => ({ title: item, icon: 'Template' }))
+      .sort(
+        (a, b) =>
+          a.title.length - b.title.length || a.title.localeCompare(b.title)
+      );
   }
 }
