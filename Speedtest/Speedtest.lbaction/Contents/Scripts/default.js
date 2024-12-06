cae6bc51f2e8216ec59b3cb00f84be55a6be314c
Ptujec <derbenderchristian@gmail.com>
2024-12-06 12:58:14 +0100

    Speedtest 1.2.1
    - some code cleanup

--- Speedtest/Speedtest.lbaction/Contents/Scripts/default.js
+++ Speedtest/Speedtest.lbaction/Contents/Scripts/default.js
@@ -1,50 +1,42 @@
-// Speedtest by Christian Bender @ptujec
-// macOS 12 required
-// Some background: https://danpetrov.xyz/macos/2021/11/14/analysing-network-quality-macos.html
+/* 
+Speedtest Action for LaunchBar
+by Christian Bender (@ptujec)
+2024-12-06
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
 
 function run() {
   if (LaunchBar.options.alternateKey) {
-    LaunchBar.executeAppleScript(
+    return LaunchBar.executeAppleScript(
       'tell application "Terminal"',
       'do script "networkQuality"',
       'activate',
       'end tell'
     );
-  } else {
-    LaunchBar.displayNotification({
-      title: 'Speedtest',
-      string: 'Test started … may take a few seconds',
-    });
-
-    var noti = LaunchBar.execute('/usr/bin/networkQuality').trim().split('\n');
-
-    var up = noti[1] // Upload capacity
-      .match(/\d+\.\d+/);
-
-    up = Math.round(up).toFixed() + ' Mbps';
-
-    var down = noti[2] // Download capacity
-      .match(/\d+\.\d+/);
-
-    down = Math.round(down).toFixed() + ' Mbps';
-
-    const osVersion = LaunchBar.systemVersion.match(/^\d\d/);
-
-    if (osVersion > 12) {
-      var resp = noti[3] // Responsiveness
-        .trim();
-    } else {
-      var resp = noti[5] // Responsiveness
-        .trim();
-    }
-
-    LaunchBar.executeAppleScript(
-      'do shell script "afplay /System/Library/Components/CoreAudio.component/Contents/SharedSupport/SystemSounds/system/acknowledgment_sent.caf"'
-    ); // Play Sound
-
-    LaunchBar.displayNotification({
-      title: 'Speedtest',
-      string: '▼ ' + down + ' ▲ ' + up + '\n' + resp,
-    });
   }
+
+  LaunchBar.displayNotification({
+    title: 'Speedtest',
+    string: 'Test started … may take a few seconds',
+  });
+
+  const output = LaunchBar.execute('/usr/bin/networkQuality')
+    .trim()
+    .split('\n');
+
+  const up = Math.round(output[1].match(/\d+\.\d+/)[0]).toFixed() + ' Mbps';
+  const down = Math.round(output[2].match(/\d+\.\d+/)[0]).toFixed() + ' Mbps';
+
+  const osVersion = LaunchBar.systemVersion.match(/^\d\d/);
+  const resp = osVersion > 12 ? output[3].trim() : output[5].trim();
+
+  LaunchBar.executeAppleScript(
+    'do shell script "afplay /System/Library/Components/CoreAudio.component/Contents/SharedSupport/SystemSounds/system/acknowledgment_sent.caf"'
+  );
+
+  LaunchBar.displayNotification({
+    title: 'Speedtest',
+    string: '▼ ' + down + ' ▲ ' + up + '\n' + resp,
+  });
 }
