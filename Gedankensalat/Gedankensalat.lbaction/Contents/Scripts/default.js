3b6cef24f7088ff102696a6c6fb9a368a38054eb
Ptujec <derbenderchristian@gmail.com>
2024-12-07 22:13:35 +0100

    Gedankensalat 2
    I made it a little simpler… because that is sometimes better.  Add items with `space`… remove them with `command` + `return`… do everything else with all the functionality that already comes with LaunchBar or other actions.
    I also added proper localization.

--- Gedankensalat/Gedankensalat.lbaction/Contents/Scripts/default.js
+++ Gedankensalat/Gedankensalat.lbaction/Contents/Scripts/default.js
@@ -1,268 +1,223 @@
-/* Gedankensalat
-Idee: Braindump für alles mögliche … hier eintragen und von hier aus weiter verwenden
+/* 
+Scratchpad Action for LaunchBar
+by Christian Bender (@ptujec)
+2024-12-07
 
-Quellen:
--  https://ia.net/writer/support/general/urlschemes
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
 
-Todo: 
+TODO: 
+- Create a matching shortcut for iOS
 - Restore removed items from backup
 */
 
+String.prototype.localizationTable = 'default';
+
 function run(argument) {
-    var fileLocation = Action.preferences.fileLocation
+  const fileLocation = Action.preferences.fileLocation;
 
-    if (fileLocation == undefined || LaunchBar.options.shiftKey) {
-        var output = fileLocationOptions()
-        return output
-    }
+  if (!fileLocation || LaunchBar.options.alternateKey)
+    return fileLocationOptions();
 
-    if (argument == undefined) { // Show Entries
-        if (LaunchBar.options.controlKey) {
-            // LaunchBar.openURL('ia-writer://open?path=/Locations/iCloud/Gedankensalat.txt')
-            LaunchBar.openURL(File.fileURLForPath(fileLocation))
-        } else {
-            var output = show()
-            return output
-        }
-    } else { // New Entry
-        var timestamp = new Date()
+  if (!argument)
+    return LaunchBar.options.controlKey
+      ? LaunchBar.openURL(File.fileURLForPath(fileLocation))
+      : show();
 
-        var date = new Date(timestamp.getTime() - (timestamp.getTimezoneOffset() * 60000))
-            .toISOString()
-            .split('T')[0]
+  // New Entry
+  let timestamp = new Date();
 
-        var time = LaunchBar.formatDate(timestamp, {
-            timeStyle: 'medium',
-            dateStyle: 'none'
-        });
+  let date = new Date(
+    timestamp.getTime() - timestamp.getTimezoneOffset() * 60000
+  )
+    .toISOString()
+    .split('T')[0];
 
-        timestamp = date + ', ' + time
+  const time = LaunchBar.formatDate(timestamp, {
+    timeStyle: 'medium',
+    dateStyle: 'none',
+  });
 
-        var text = File.readText(fileLocation).trim();
+  timestamp = `${date}, ${time}`;
 
-        text = text + '\n\n' + timestamp + ': ' + argument
+  const text = `${File.readText(
+    fileLocation
+  ).trim()}\n\n${timestamp}: ${argument}`;
 
-        File.writeText(text, fileLocation);
+  File.writeText(text, fileLocation);
 
-        LaunchBar.hide();
-    }
-    // var output = show()
-    // return output
+  LaunchBar.hide();
+  // return show();
 }
 
 function fileLocationOptions() {
-    if (LaunchBar.currentLocale == 'de') {
-        var chooseTitle = 'Datei auswählen'
-        var newTitle = 'Neue Datei erstellen'
-    } else {
-        var chooseTitle = 'Choose File'
-        var newTitle = 'Create New File'
-    }
-
-    return [
-        {
-            title: chooseTitle,
-            action: 'chooseFile',
-            icon: 'fileTemplate'
-        }, {
-            title: newTitle,
-            action: 'newFile',
-            icon: 'newFileTemplate'
-        }
-    ]
+  return [
+    {
+      title: 'Choose File'.localize(),
+      action: 'chooseFile',
+      icon: 'fileTemplate',
+    },
+    {
+      title: 'Create New File'.localize(),
+      action: 'newFile',
+      icon: 'newFileTemplate',
+    },
+  ];
 }
 
 function chooseFile() {
-    if (LaunchBar.currentLocale == 'de') {
-        var prompt = 'Wähle eine Datei für diese Aktion:'
-        var success = 'Fertig!'
-        var successMessage1 = 'Die ausgewählte Datei ist '
-        var successMessage2 = 'Jetzt kanns losgehn!'
-        var error = 'Fehler'
-        var errorMessage = 'Keine Datei. Versuch es nochmal!'
-    } else {
-        var prompt = 'Select a file for this action:'
-        var success = 'Success!'
-        var successMessage1 = 'Your source file is '
-        var successMessage2 = 'You can start adding entries.'
-        var error = 'Error'
-        var errorMessage = 'No File. Try again!'
-    }
+  var fileLocation = LaunchBar.executeAppleScript(
+    'set _home to path to home folder as string',
+    'set _default to _home & "Library:Mobile Documents:" as alias',
+    'set _file to choose file with prompt "' +
+      'Select a file for this action:'.localize() +
+      '" default location _default',
+    'set _file to POSIX path of _file'
+  ).trim();
+  Action.preferences.fileLocation = fileLocation;
 
-    var fileLocation = LaunchBar.executeAppleScript(
-        'set _home to path to home folder as string',
-        'set _default to _home & "Library:Mobile Documents:" as alias',
-        'set _file to choose file with prompt "' + prompt + '" default location _default',
-        'set _file to POSIX path of _file')
-        .trim()
-    Action.preferences.fileLocation = fileLocation
-
-    if (Action.preferences.fileLocation != '') {
-        LaunchBar.alert(success, successMessage1 + fileLocation + '\n' + successMessage2)
-    } else {
-        LaunchBar.alert(error, errorMessage)
-    }
+  if (Action.preferences.fileLocation != '') {
+    LaunchBar.alert(
+      'Success!'.localize(),
+      'Your source file is '.localize() +
+        fileLocation +
+        '\n' +
+        'You can start adding entries.'.localize()
+    );
+  } else {
+    LaunchBar.alert('Error'.localize(), 'No File. Try again!'.llocal);
+  }
 }
 
 function newFile() {
-    if (LaunchBar.currentLocale == 'de') {
-        var prompt = 'Wähle einen Ordner, in dem die neue Datei erstellt werden soll:'
-        var success = 'Fertig!'
-        var successMessage1 = 'Die ausgewählte Datei ist '
-        var successMessage2 = 'Jetzt kanns losgehn!'
-        var header = '# Gedankensalat'
-        var fileName = 'Gedankensalat.txt'
-        var error = 'Fehler'
-        var errorMessage = 'Keine Datei. Versuch es nochmal!'
+  const folderPath = LaunchBar.executeAppleScript(
+    'set _home to path to home folder as string',
+    'set _default to _home & "Library:Mobile Documents:" as alias',
+    'set _folder to choose folder with prompt "' +
+      'Select a folder to create the new file in:'.localize() +
+      '" default location _default',
+    'set _folder to POSIX path of _folder'
+  ).trim();
+
+  const fileLocation = folderPath + 'Scratchpad.txt'.localize();
+
+  try {
+    File.writeText('# Scratchpad'.localize(), fileLocation);
+    Action.preferences.fileLocation = fileLocation;
+
+    if (Action.preferences.fileLocation != '') {
+      LaunchBar.alert(
+        'Success!'.localize(),
+        'Your source file is '.localize() +
+          fileLocation +
+          '\n' +
+          'You can start adding entries.'.localize()
+      );
     } else {
-        var prompt = 'Select a folder to create the new file in:'
-        var success = 'Success!'
-        var successMessage1 = 'Your source file is '
-        var successMessage2 = 'You can start adding entries.'
-        var header = '# Scratchpad'
-        var fileName = 'Scratchpad.txt'
-        var error = 'Error'
-        var errorMessage = 'No File. Try again!'
+      LaunchBar.alert('Error'.localize(), 'No File. Try again!'.localize());
     }
-
-    var folderPath = LaunchBar.executeAppleScript(
-        'set _home to path to home folder as string',
-        'set _default to _home & "Library:Mobile Documents:" as alias',
-        'set _folder to choose folder with prompt "' + prompt + '" default location _default',
-        'set _folder to POSIX path of _folder')
-        .trim()
-
-    var fileLocation = folderPath + fileName
-
-    try {
-        File.writeText(header, fileLocation);
-        Action.preferences.fileLocation = fileLocation
-
-        if (Action.preferences.fileLocation != '') {
-            LaunchBar.alert(success, successMessage1 + fileLocation + '\n' + successMessage2)
-        } else {
-            LaunchBar.alert(error, errorMessage)
-        }
-    } catch (exception) {
-        LaunchBar.alert('Error while writing text to file: ' + exception);
-    }
+  } catch (exception) {
+    LaunchBar.alert(
+      'Error while writing text to file: '.localize() + exception
+    );
+  }
 }
 
 function show() {
-    var fileLocation = Action.preferences.fileLocation
+  const fileLocation = Action.preferences.fileLocation;
 
-    try {
-        var lines = File.readText(fileLocation)
-            .split('\n')
-    } catch (error) {
-        var output = fileLocationOptions()
-        return output
-    }
+  let lines;
+  try {
+    lines = File.readText(fileLocation).split('\n');
+  } catch (e) {
+    return fileLocationOptions();
+  }
 
-    var result = []
-    for (var i = 1; i < lines.length; i++) {
-        if (lines[i] != '') {
-            var l = lines[i].split(/\d\d: /)
-
-            result.push({
-                title: l[1],
-                subtitle: l[0].replace(/:$/, ''),
-                icon: '_Template',
-                action: 'options',
-                actionArgument: {
-                    entry: lines[i],
-                    title: l[1]
-                }
-            })
-        }
-    }
-
-    result.sort(function (a, b) {
-        return a.subtitle < b.subtitle;
-    });
-
-    return result
+  return lines
+    .filter((line) => line !== '')
+    .map((line) => ({
+      title: line.split(/\d\d: /)[1],
+      subtitle: line.split(/\d\d: /)[0].replace(/:$/, ''),
+      icon: '_Template',
+      action: 'action',
+      actionArgument: {
+        line,
+        title: line.split(/\d\d: /)[1],
+      },
+      alwaysShowsSubtitle: true,
+    }))
+    .reverse();
 }
 
-function options(params) {
-    var fileLocation = Action.preferences.fileLocation
+function action({ line, title }) {
+  // Paste
+  if (LaunchBar.options.shiftKey) LaunchBar.paste(title);
 
-    if (LaunchBar.options.shiftKey) {
-        // LaunchBar.openURL('ia-writer://open?path=/Locations/iCloud/Gedankensalat.txt')
-        LaunchBar.openURL(File.fileURLForPath(fileLocation))
-    } else {
-        if (LaunchBar.currentLocale == 'de') {
-            var deleteTitle = 'Löschen'
-            var pasteTitle = 'Einfügen'
-        } else {
-            var deleteTitle = 'Delete'
-            var pasteTitle = 'Paste'
-        }
-        
-        return [
-            {
-                title: 'Todoist',
-                icon: 'todoistTemplate',
-                action: 'addToTodoist',
-                actionArgument: params.title
-            }, {
-                title: 'MindNode',
-                icon: 'mindnodeTemplate',
-                action: 'addToMindNode',
-                actionArgument: params.title
-            }, {
-                title: 'iA Writer',
-                icon: 'iATemplate',
-                action: 'addToIAWriter',
-                actionArgument: params.title
-            }, {
-                title: 'Day One',
-                icon: 'dayoneTemplate',
-                action: 'addToDayOne',
-                actionArgument: params.title
-            }, {
-                title: pasteTitle,
-                icon: 'CopyActionTemplate',
-                action: 'paste',
-                actionArgument: params.title
-            }, {
-                title: deleteTitle,
-                icon: 'removeTemplate',
-                action: 'remove',
-                actionArgument: params.entry
-            }
-        ]
-    }
+  // Delete
+  if (LaunchBar.options.commandKey) {
+    const fileLocation = Action.preferences.fileLocation;
+    const text = File.readText(fileLocation)
+      .replace(line, '')
+      .replace(/\n\n(\n)+/, '\n\n');
+
+    File.writeText(text, fileLocation);
+    return show();
+  }
+
+  LaunchBar.displayInLargeType({ string: title });
+}
+
+function options(sEntry, sTitle) {
+  const fileLocation = Action.preferences.fileLocation;
+
+  if (LaunchBar.options.shiftKey)
+    return LaunchBar.openURL(File.fileURLForPath(fileLocation));
+
+  return [
+    {
+      title: 'Todoist',
+      icon: 'todoistTemplate',
+      action: 'addToTodoist',
+      actionArgument: sTitle,
+    },
+    {
+      title: 'MindNode',
+      icon: 'mindnodeTemplate',
+      action: 'addToMindNode',
+      actionArgument: sTitle,
+    },
+    {
+      title: 'iA Writer',
+      icon: 'iATemplate',
+      action: 'addToIAWriter',
+      actionArgument: sTitle,
+    },
+    {
+      title: 'Day One',
+      icon: 'dayoneTemplate',
+      action: 'addToDayOne',
+      actionArgument: sTitle,
+    },
+    {
+      title: 'Paste'.localize(),
+      icon: 'CopyActionTemplate',
+      action: 'paste',
+      actionArgument: sTitle,
+    },
+    {
+      title: 'Delete'.localize(),
+      icon: 'removeTemplate',
+      action: 'remove',
+      actionArgument: sEntry,
+    },
+  ];
 }
 
 function remove(params) {
-    var fileLocation = Action.preferences.fileLocation
-    var text = File.readText(fileLocation)
-        .replace(params, '')
-        .replace(/\n\n(\n)+/, '\n\n')
+  const fileLocation = Action.preferences.fileLocation;
+  const text = File.readText(fileLocation)
+    .replace(params, '')
+    .replace(/\n\n(\n)+/, '\n\n');
 
-    File.writeText(text, fileLocation);
-
-    var output = show()
-    return output
-}
-
-function paste(params) {
-    LaunchBar.paste(params)
-}
-
-function addToTodoist(params) {
-    LaunchBar.openURL('todoist://addtask?content=' + encodeURI(params))
-}
-
-function addToDayOne(params) {
-    LaunchBar.openURL('dayone://post?entry=' + encodeURI(params) + '&tags=Gedankensalat&journal=Privat')
-}
-
-function addToIAWriter(params) {
-    LaunchBar.openURL('ia-writer://new?text=' + encodeURIComponent(params) + '&new-window=true');
-}
-
-function addToMindNode(params) {
-    LaunchBar.openURL('mindnode://import?format=txt&name=' + encodeURIComponent(params) + '&content=' + encodeURIComponent(params));
+  File.writeText(text, fileLocation);
+  return show();
 }
--- Gedankensalat/README.md
+++ Gedankensalat/README.md
@@ -1,20 +1,21 @@
 # LaunchBar Action: Gedankensalat (Scratchpad)
 
-The use-case of this is for when you working on something and a random thought comes to your mind. As a LaunchBar user hitting `⌘ ␣` (command-space), `␣` (space) and type away should be the quickest way to capture a random thought. This is what this action is for. 
+The use case for this is when you are working on something, and a random thought comes to your mind. As a LaunchBar user, hitting `command` + `space` and typing away should be the quickest way to capture a random thought. This is what this action is for.
+
+Just select the action, press `space` again, and write down what's on your mind, then keep doing what you are doing.
 
 <img src="01.png" width="600"/>
 
-Write the thought down and keep doing what you are doing. 
+Later, you can go back and decide what to do with it.
 
 <img src="02.png" width="600"/>
 
-Later you can go back and decide what to do with it.
+You can easily remove items with `command` + `return`, or you can pass them on to an app or another action.
 
-<img src="03.png" width="600"/>
+## Settings
 
-The text is stored in a simple plain text file you can store wherever it suits you.
+The items are stored in a simple text file. You can choose a file or create a new file with `option` + `return`.
 
-<img src="04.png" width="600"/>
 
 ## Download
 [Click here](https://github.com/Ptujec/LaunchBar/archive/refs/heads/master.zip) to download this LaunchBar action along with all the others. Or [clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository.   
