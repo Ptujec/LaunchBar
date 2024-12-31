90ece488140d85bbf87d0783ff9f65c776f8e80e
Ptujec <derbenderchristian@gmail.com>
2024-12-31 17:18:28 +0100

    Gedankensalat 2.0.1
    - Fixing issue #37 (you might need to start fresh to have things displayed correctly).
    - Showing correct settings shortcut in summary.
    - Clean up (removed old script versions and unnecessary code).
    - Updated file icons.
    - README/screenshots update.
    - Added at least some error logging.
    - Alert when the file has no entry.

--- Gedankensalat/Gedankensalat.lbaction/Contents/Scripts/default.js
+++ Gedankensalat/Gedankensalat.lbaction/Contents/Scripts/default.js
@@ -24,29 +24,14 @@ function run(argument) {
       : show();
 
   // New Entry
-  let timestamp = new Date();
+  let timestamp = new Date().toISOString();
 
-  let date = new Date(
-    timestamp.getTime() - timestamp.getTimezoneOffset() * 60000
-  )
-    .toISOString()
-    .split('T')[0];
-
-  const time = LaunchBar.formatDate(timestamp, {
-    timeStyle: 'medium',
-    dateStyle: 'none',
-  });
-
-  timestamp = `${date}, ${time}`;
-
-  const text = `${File.readText(
-    fileLocation
-  ).trim()}\n\n${timestamp}: ${argument}`;
+  const text = `${File.readText(fileLocation).trim()}\n${timestamp} ${argument
+    .replace(/\n/g, ' ')
+    .trim()}`;
 
   File.writeText(text, fileLocation);
-
   LaunchBar.hide();
-  // return show();
 }
 
 function fileLocationOptions() {
@@ -116,9 +101,7 @@ function newFile() {
       LaunchBar.alert('Error'.localize(), 'No File. Try again!'.localize());
     }
   } catch (exception) {
-    LaunchBar.alert(
-      'Error while writing text to file: '.localize() + exception
-    );
+    LaunchBar.log('Error while writing text to file: '.localize() + exception);
   }
 }
 
@@ -129,95 +112,56 @@ function show() {
   try {
     lines = File.readText(fileLocation).split('\n');
   } catch (e) {
+    LaunchBar.log('Error while reading file: ' + e);
     return fileLocationOptions();
   }
 
-  return lines
-    .filter((line) => line !== '')
-    .map((line) => ({
-      title: line.split(/\d\d: /)[1],
-      subtitle: line.split(/\d\d: /)[0].replace(/:$/, ''),
-      icon: '_Template',
-      action: 'action',
-      actionArgument: {
-        line,
-        title: line.split(/\d\d: /)[1],
-      },
-      alwaysShowsSubtitle: true,
-    }))
+  const results = lines
+    .filter((line) => line !== '' && !line.startsWith('#'))
+    .map((line) => {
+      let title, timestamp;
+
+      try {
+        const components = line.split(' ');
+        title = components.slice(1).join(' ');
+        timestamp = components[0];
+      } catch (e) {
+        LaunchBar.log('Error while parsing line: ' + e);
+        return null;
+      }
+
+      const subtitle = LaunchBar.formatDate(new Date(timestamp), {
+        timeStyle: 'medium',
+        dateStyle: 'short',
+      });
+
+      return {
+        title,
+        subtitle,
+        icon: '_Template',
+        action: 'action',
+        actionArgument: { line, title },
+        alwaysShowsSubtitle: true,
+      };
+    })
     .reverse();
+
+  return results.length > 0
+    ? results
+    : { title: 'No entries'.localize(), icon: 'alert' };
 }
 
 function action({ line, title }) {
   // Paste
-  if (LaunchBar.options.shiftKey) LaunchBar.paste(title);
+  if (LaunchBar.options.shiftKey) return LaunchBar.paste(title);
 
   // Delete
   if (LaunchBar.options.commandKey) {
     const fileLocation = Action.preferences.fileLocation;
-    const text = File.readText(fileLocation)
-      .replace(line, '')
-      .replace(/\n\n(\n)+/, '\n\n');
-
+    const text = File.readText(fileLocation).replace(line, '');
     File.writeText(text, fileLocation);
     return show();
   }
 
   LaunchBar.displayInLargeType({ string: title });
-}
-
-function options(sEntry, sTitle) {
-  const fileLocation = Action.preferences.fileLocation;
-
-  if (LaunchBar.options.shiftKey)
-    return LaunchBar.openURL(File.fileURLForPath(fileLocation));
-
-  return [
-    {
-      title: 'Todoist',
-      icon: 'todoistTemplate',
-      action: 'addToTodoist',
-      actionArgument: sTitle,
-    },
-    {
-      title: 'MindNode',
-      icon: 'mindnodeTemplate',
-      action: 'addToMindNode',
-      actionArgument: sTitle,
-    },
-    {
-      title: 'iA Writer',
-      icon: 'iATemplate',
-      action: 'addToIAWriter',
-      actionArgument: sTitle,
-    },
-    {
-      title: 'Day One',
-      icon: 'dayoneTemplate',
-      action: 'addToDayOne',
-      actionArgument: sTitle,
-    },
-    {
-      title: 'Paste'.localize(),
-      icon: 'CopyActionTemplate',
-      action: 'paste',
-      actionArgument: sTitle,
-    },
-    {
-      title: 'Delete'.localize(),
-      icon: 'removeTemplate',
-      action: 'remove',
-      actionArgument: sEntry,
-    },
-  ];
-}
-
-function remove(params) {
-  const fileLocation = Action.preferences.fileLocation;
-  const text = File.readText(fileLocation)
-    .replace(params, '')
-    .replace(/\n\n(\n)+/, '\n\n');
-
-  File.writeText(text, fileLocation);
-  return show();
 }
--- Gedankensalat/README.md
+++ Gedankensalat/README.md
@@ -4,18 +4,20 @@ The use case for this is when you are working on something, and a random thought
 
 Just select the action, press `space` again, and write down what's on your mind, then keep doing what you are doing.
 
-<img src="01.png" width="600"/>
+<img src="01.jpg" width="794"/>
 
-Later, you can go back and decide what to do with it.
+Later, you can go back and decide what to do with it. Hit `enter` to view your list of entries.
 
-<img src="02.png" width="600"/>
+<img src="02.jpg" width="794"/>
 
-You can easily remove items with `command` + `return`, or you can pass them on to an app or another action.
+You can easily remove items with `command` + `return`, or you can pass them on to an app or another action. 
+
+BTW, `control` + `return` will open the text file in your default editor.
 
 ## Settings
 
 The items are stored in a simple text file. You can choose a file or create a new file with `option` + `return`.
 
-
 ## Download
-[Click here](https://github.com/Ptujec/LaunchBar/archive/refs/heads/master.zip) to download this LaunchBar action along with all the others. Or [clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository.   
+
+[Click here](https://github.com/Ptujec/LaunchBar/archive/refs/heads/master.zip) to download this LaunchBar action along with all the others. Or [clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository.
