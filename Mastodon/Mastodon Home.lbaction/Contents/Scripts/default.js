836339893a8bdb1e42951bd2cc09b117a67c29f5
Ptujec <derbenderchristian@gmail.com>
2025-01-11 11:58:10 +0100

    Mastodon Home 1.4.3
    refactoring

--- Mastodon/Mastodon Home.lbaction/Contents/Scripts/default.js
+++ Mastodon/Mastodon Home.lbaction/Contents/Scripts/default.js
@@ -1,7 +1,7 @@
 /* 
 Mastodon Home Action for LaunchBar
 by Christian Bender (@ptujec)
-2023-01-20
+2025-01-11
 
 Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
 
@@ -14,229 +14,205 @@ Test: https://mstdn.plus/@lapcatsoftware/109718227833446585
 */
 String.prototype.localizationTable = 'default';
 
-function run() {
-  var apiToken = Action.preferences.apiToken;
-  var server = Action.preferences.server;
-  var closeOption = Action.preferences.closeOption;
+const SUPPORTED_BROWSERS = [
+  'Brave Browser',
+  'Google Chrome',
+  'Arc',
+  'Vivaldi',
+  'Safari',
+];
+const URL_SCHEMES = {
+  ivory: 'ivory://',
+  mammoth: 'mammoth://',
+  icecubes: 'icecubesapp://',
+  web: 'https://',
+};
 
-  // Set Mastodon Server/Instance
-  if (server == undefined || server == '') {
-    setInstance(server);
-    return;
-  }
+function getBrowserUrlScript(browser) {
+  return browser === 'Safari'
+    ? 'tell application "Safari" to set _url to URL of front document'
+    : `tell application "${browser}" to set _url to URL of active tab of front window`;
+}
 
-  if (LaunchBar.options.shiftKey) {
-    var output = settings(server, apiToken);
-    return output;
-  }
-
-  // Get frontmost app
-  var frontmost = LaunchBar.executeAppleScript(
-    'tell application "System Events" to set _frontmoste to name of application processes whose frontmost is true as string'
-  ).trim();
-
-  // Broswer specific Applescripts
-  if (
-    frontmost == 'Brave Browser' ||
-    frontmost == 'Google Chrome' ||
-    frontmost == 'Arc' ||
-    frontmost == 'Vivaldi'
-  ) {
-    var getUrlAS =
-      'tell application "' +
-      frontmost +
-      '" to set _url to URL of active tab of front window';
-  } else if (frontmost == 'Safari') {
-    var getUrlAS =
-      'tell application "Safari" to set _url to URL of front document';
-  } else {
-    LaunchBar.alert(frontmost + ' is not a supported browser!'.localize());
-    return;
-  }
-
-  // Get current website from Safari
-  var url = LaunchBar.executeAppleScript(getUrlAS).trim();
-
-  if (url == 'missing value' || url == 'favorites://' || url == '') {
+function validateUrl(url) {
+  if (url === 'missing value' || url === 'favorites://' || url === '') {
     LaunchBar.alert('No current website found!'.localize());
     LaunchBar.hide();
-    return;
+    return null;
   }
 
   if (!url.includes('@')) {
     LaunchBar.alert('Seems to be no valid account or post!'.localize());
     LaunchBar.hide();
+    return null;
+  }
+
+  const parts = url.split('/');
+  return {
+    instance: parts[2],
+    user: parts[3],
+    unknown: parts[4],
+  };
+}
+
+function run() {
+  const apiToken = Action.preferences.apiToken;
+  const server = Action.preferences.server;
+  const closeOption = Action.preferences.closeOption;
+
+  if (!server) return setInstance(server);
+  if (LaunchBar.options.shiftKey) return settings(server, apiToken);
+
+  const frontmost = LaunchBar.executeAppleScript(
+    'tell application "System Events" to set _frontmoste to name of application processes whose frontmost is true as string'
+  ).trim();
+
+  if (!SUPPORTED_BROWSERS.includes(frontmost)) {
+    LaunchBar.alert(frontmost + ' is not a supported browser!'.localize());
     return;
   }
 
-  var res = url.split('/');
-  var instance = res[2];
-  var user = res[3];
-  var unknown = res[4];
+  const url = LaunchBar.executeAppleScript(
+    getBrowserUrlScript(frontmost)
+  ).trim();
+  const urlComponents = validateUrl(url);
+  if (!urlComponents) return;
 
-  // Check client setting
-  if (Action.preferences.openIn != true) {
-    var urlscheme = 'https://';
-  } else {
-    var urlscheme = Action.preferences.openInURLScheme;
-  }
+  const urlscheme = Action.preferences.openIn
+    ? Action.preferences.openInURLScheme
+    : URL_SCHEMES.web;
 
-  // Check if url includes home instance/server
   if (url.includes(server)) {
     LaunchBar.alert('Your are already at home!'.localize());
     LaunchBar.hide();
     return;
   }
 
-  // Ivory
-  if (urlscheme == 'ivory://') {
+  if (urlscheme === URL_SCHEMES.ivory) {
     LaunchBar.openURL(urlscheme + 'acct/openURL?url=' + url);
-    if (closeOption == 'true') {
-      closeURL(url, frontmost);
-    }
+    if (closeOption === 'true') closeURL(url, frontmost);
     return;
   }
 
-  var homeURL = urlscheme + server + '/' + user + '@' + instance;
+  const homeURL = `${urlscheme}${server}/${urlComponents.user}@${urlComponents.instance}`;
+  const isPostId = !isNaN(parseInt(urlComponents.unknown));
 
-  var isNoPostId = isNaN(parseInt(unknown));
-
-  if (isNoPostId == false) {
-    // Post/Status URL
-    var statusData = HTTP.getJSON(
-      'https://' +
-        server +
-        '/api/v2/search?q=' +
-        url +
-        '&type=statuses&resolve=true',
-      {
-        headerFields: {
-          Authorization: 'Bearer ' + apiToken,
-        },
-      }
-    );
-
-    //  Error Message
-    if (statusData.response.status != 200) {
-      LaunchBar.alert(
-        'Error: ' + statusData.response.status,
-        statusData.response.localizedStatus
-      );
-      return;
-    }
-
-    // File.writeJSON(statusData, Action.supportPath + '/test.json');
-    // return;
-
-    var id = statusData.data.statuses[0].id;
-
-    homeURL = homeURL + '/' + id;
-
-    LaunchBar.hide();
-    LaunchBar.openURL(homeURL, frontmost);
+  if (isPostId) {
+    handlePostUrl(homeURL, server, apiToken, url, frontmost, closeOption);
   } else {
-    // Account URL
+    handleAccountUrl(
+      homeURL,
+      server,
+      apiToken,
+      url,
+      frontmost,
+      closeOption,
+      urlscheme
+    );
+  }
+}
 
-    LaunchBar.hide();
-
-    // Fix for Mammoth
-    if (urlscheme == 'mammoth://') {
-      LaunchBar.openURL(url.replace('https://', urlscheme));
-      if (closeOption == 'true') {
-        closeURL(url, frontmost);
-      }
-      return;
+function handlePostUrl(homeURL, server, apiToken, url, frontmost, closeOption) {
+  const statusData = HTTP.getJSON(
+    `https://${server}/api/v2/search?q=${url}&type=statuses&resolve=true`,
+    {
+      headerFields: { Authorization: `Bearer ${apiToken}` },
     }
+  );
 
-    LaunchBar.openURL(homeURL, frontmost);
-
-    // Check homeURL via API … if not correct replace it -- it's faster for most accounts not using the API straight away. But some won't work like e.g. https://mastodon.macstories.net/@viticci
-
-    // Exception for Ice Cubes … not necessary
-    if (urlscheme == 'icecubesapp://') {
-      if (closeOption == 'true') {
-        closeURL(url, frontmost);
-      }
-      return;
-    }
-
-    if (apiToken == undefined) {
-      setApiKey();
-      return;
-    }
-
-    var searchURL =
-      'https://' +
-      server +
-      '/api/v2/search?q=' +
-      encodeURIComponent(url) +
-      '&type=accounts&resolve=true';
-
-    var accountData = HTTP.getJSON(searchURL, {
-      headerFields: {
-        Authorization: 'Bearer ' + apiToken,
-      },
-    });
-
-    //  Error Message
-    if (accountData.response.status != 200) {
-      LaunchBar.alert(
-        'Error: ' + accountData.response.status,
-        accountData.response.localizedStatus
-      );
-      return;
-    }
-
-    // File.writeJSON(accountData, Action.supportPath + '/test.json');
-    // return;
-
-    var acct = accountData.data.accounts[0].acct;
-    var homeURLAPI = urlscheme + server + '/' + '@' + acct;
-
-    if (homeURL != homeURLAPI) {
-      LaunchBar.openURL(homeURLAPI, frontmost);
-      closeURL(homeURL, frontmost);
-    }
+  if (statusData.response.status !== 200) {
+    LaunchBar.alert(
+      `Error: ${statusData.response.status}`,
+      statusData.response.localizedStatus
+    );
+    return;
   }
 
-  if (closeOption == 'true') {
-    closeURL(url, frontmost);
+  const finalUrl = `${homeURL}/${statusData.data.statuses[0].id}`;
+  LaunchBar.hide();
+  LaunchBar.openURL(finalUrl, frontmost);
+
+  if (closeOption === 'true') closeURL(url, frontmost);
+}
+
+function handleAccountUrl(
+  homeURL,
+  server,
+  apiToken,
+  url,
+  frontmost,
+  closeOption,
+  urlscheme
+) {
+  LaunchBar.hide();
+
+  if (urlscheme === URL_SCHEMES.mammoth) {
+    LaunchBar.openURL(url.replace('https://', urlscheme));
+    if (closeOption === 'true') closeURL(url, frontmost);
+    return;
   }
-  return;
+
+  LaunchBar.openURL(homeURL, frontmost);
+
+  if (urlscheme === URL_SCHEMES.icecubes) {
+    if (closeOption === 'true') closeURL(url, frontmost);
+    return;
+  }
+
+  if (!apiToken) return setApiKey();
+
+  const searchURL = `https://${server}/api/v2/search?q=${encodeURIComponent(
+    url
+  )}&type=accounts&resolve=true`;
+  const accountData = HTTP.getJSON(searchURL, {
+    headerFields: { Authorization: `Bearer ${apiToken}` },
+  });
+
+  if (accountData.response.status !== 200) {
+    LaunchBar.alert(
+      `Error: ${accountData.response.status}`,
+      accountData.response.localizedStatus
+    );
+    return;
+  }
+
+  const acct = accountData.data.accounts[0].acct;
+  const homeURLAPI = `${urlscheme}${server}/@${acct}`;
+
+  if (homeURL !== homeURLAPI) {
+    LaunchBar.openURL(homeURLAPI, frontmost);
+    closeURL(homeURL, frontmost);
+  }
+
+  if (closeOption === 'true') closeURL(url, frontmost);
 }
 
 function settings() {
-  var server = Action.preferences.server;
+  const server = Action.preferences.server;
+  const openInLabel = Action.preferences.openIn
+    ? 'Opens: '.localize() + Action.preferences.openInName
+    : 'Open: Website'.localize();
+  const openIcon = Action.preferences.openIn
+    ? Action.preferences.openInIcon
+    : 'safariTemplate';
 
-  if (Action.preferences.openIn != true) {
-    var openInLabel = 'Open: Website'.localize();
-    var openIcon = 'safariTemplate';
-  } else {
-    var openInLabel = 'Opens: '.localize() + Action.preferences.openInName;
-    var openIcon = Action.preferences.openInIcon;
-  }
+  const { xArg, xIcon, label } =
+    Action.preferences.closeOption !== 'true'
+      ? { xArg: 'true', xIcon: 'xOffTemplate', label: 'Off'.localize() }
+      : { xArg: 'false', xIcon: 'xTemplate', label: 'On'.localize() };
 
-  if (Action.preferences.closeOption != 'true') {
-    var xArg = 'true';
-    var xIcon = 'xOffTemplate';
-    var label = 'Off'.localize();
-  } else {
-    var xArg = 'false';
-    var xIcon = 'xTemplate';
-    var label = 'On'.localize();
-  }
-
-  options = [
+  return [
     {
       title: 'Close Original Site'.localize(),
       action: 'closeOriginalToggle',
       actionArgument: xArg,
-      label: label,
+      label,
       icon: xIcon,
     },
     {
       title: 'Open'.localize(),
       action: 'openSetting',
+      actionReturnsItems: true,
       label: openInLabel,
       icon: openIcon,
     },
@@ -253,60 +229,43 @@ function settings() {
       icon: 'keyTemplate',
     },
   ];
-
-  return options;
 }
 
 function closeOriginalToggle(xArg) {
-  if (xArg == 'true') {
-    Action.preferences.closeOption = 'true';
-  } else {
-    Action.preferences.closeOption = 'false';
-  }
-
-  var output = settings();
-  return output;
+  Action.preferences.closeOption = xArg;
+  return settings();
 }
 
-function closeURL(url, frontmost) {
-  // Broswer specific Applescripts
-  if (
-    frontmost == 'Brave Browser' ||
-    frontmost == 'Google Chrome' ||
-    frontmost == 'Arc' ||
-    frontmost == 'Vivaldi'
-  ) {
-    LaunchBar.executeAppleScript(
-      'tell application "' + frontmost + '"',
-      '	repeat with _window in windows',
-      '		tell _window',
-      '			close (every tab whose URL is "' + url + '")',
-      '		end tell',
-      '	end repeat',
-      'end tell'
-    );
-  } else if (frontmost == 'Safari') {
-    LaunchBar.executeAppleScript(
-      'tell application "Safari"',
-      '	repeat with _window in windows',
-      '		set _tabs to tabs of _window',
-      '		repeat with _tab in _tabs',
-      '			if URL of _tab is "' + url + '" then',
-      '				set _tabtoclose to _tab',
-      '				exit repeat',
-      '			end if',
-      '		end repeat',
-      '	end repeat',
-      '	try',
-      '		close _tabtoclose',
-      '	end try',
-      'end tell'
-    );
-  }
+function closeURL(url, browser) {
+  const script =
+    browser === 'Safari'
+      ? `tell application "Safari"
+        repeat with _window in windows
+          set _tabs to tabs of _window
+          repeat with _tab in _tabs
+            if URL of _tab is "${url}" then
+              set _tabtoclose to _tab
+              exit repeat
+            end if
+          end repeat
+        end repeat
+        try
+          close _tabtoclose
+        end try
+      end tell`
+      : `tell application "${browser}"
+        repeat with _window in windows
+          tell _window
+            close (every tab whose URL is "${url}")
+          end tell
+        end repeat
+      end tell`;
+
+  LaunchBar.executeAppleScript(script);
 }
 
 function openSetting() {
-  options = [
+  return [
     {
       title: 'Open: Elk'.localize(),
       action: 'openIn',
@@ -368,8 +327,6 @@ function openSetting() {
       icon: 'safariTemplate',
     },
   ];
-
-  return options;
 }
 
 function openIn(dict) {
@@ -377,86 +334,70 @@ function openIn(dict) {
   Action.preferences.openInURLScheme = dict.urlscheme;
   Action.preferences.openInName = dict.name;
   Action.preferences.openInIcon = dict.icon;
-
-  var output = settings();
-  return output;
+  return settings();
 }
 
 function setApiKey() {
-  var server = Action.preferences.server;
-  var response = LaunchBar.alert(
+  const server = Action.preferences.server;
+  const response = LaunchBar.alert(
     'API-Token required',
     '1) Read the instructions on how to create an API-Token.\n2) Press "Set API-Token"\n\nThe API-Token will be stored in the action preferences (~/Library/Application Support/LaunchBar/Action Support/ptujec.LaunchBar.action.MastodonHome/Preferences.plist)',
     'Open Instructions & Mastodon Settings',
     'Set API-Token',
     'Cancel'
   );
+
   switch (response) {
     case 0:
       LaunchBar.hide();
-      LaunchBar.openURL('https://' + server + '/settings/applications');
+      LaunchBar.openURL(`https://${server}/settings/applications`);
       LaunchBar.executeAppleScript('delay 0.2');
       LaunchBar.openURL(
         'https://github.com/Ptujec/LaunchBar/tree/master/Mastodon#api-token'
       );
       break;
     case 1:
-      var clipboardContent = LaunchBar.getClipboardString().trim();
-
-      if (clipboardContent.length == 43) {
-        // Test API-Token
-        var statusData = HTTP.getJSON(
-          'https://' +
-            server +
-            '/api/v2/search?q=test&type=statuses&resolve=true',
-          {
-            headerFields: {
-              Authorization: 'Bearer ' + clipboardContent,
-            },
-          }
-        );
-
-        //  Error Message
-        if (statusData.response.status != 200) {
-          LaunchBar.alert(
-            'Error: ' + statusData.response.status,
-            statusData.response.localizedStatus
-          );
-          return;
-        } else {
-          // Write new API-Token in Action preferences
-          Action.preferences.apiToken = clipboardContent;
-
-          LaunchBar.alert(
-            'Success!',
-            'API-Token set to: ' + Action.preferences.apiToken
-          );
-        }
-      } else {
+      const clipboardContent = LaunchBar.getClipboardString().trim();
+      if (clipboardContent.length !== 43) {
         LaunchBar.alert(
           'The length of the clipboard content does not match the length of a correct API-Token',
           'Make sure the API-Token is the most recent item in the clipboard!'
         );
+        return;
       }
-      break;
-    case 2:
+
+      const statusData = HTTP.getJSON(
+        `https://${server}/api/v2/search?q=test&type=statuses&resolve=true`,
+        {
+          headerFields: { Authorization: `Bearer ${clipboardContent}` },
+        }
+      );
+
+      if (statusData.response.status !== 200) {
+        LaunchBar.alert(
+          `Error: ${statusData.response.status}`,
+          statusData.response.localizedStatus
+        );
+        return;
+      }
+
+      Action.preferences.apiToken = clipboardContent;
+      LaunchBar.alert(
+        'Success!',
+        `API-Token set to: ${Action.preferences.apiToken}`
+      );
       break;
   }
 }
 
 function setInstance(server) {
   LaunchBar.hide();
-  if (server == undefined || server == '') {
-    var defaultAnswer = 'mastodon.social';
-  } else {
-    var defaultAnswer = server;
-  }
-
-  var dialog =
+  const defaultAnswer = server || 'mastodon.social';
+  const dialog =
     'Enter the name of the Mastodon instance or server where your account is hosted!'.localize();
-  var dialogTitle = 'Mastodon Instance'.localize();
+  const dialogTitle = 'Mastodon Instance'.localize();
 
-  var server = LaunchBar.executeAppleScript(
+  const newServer = LaunchBar.executeAppleScript(
     'set result to display dialog "' +
       dialog +
       '" with title "' +
@@ -466,6 +407,6 @@ function setInstance(server) {
       '"',
     'set result to text returned of result'
   ).trim();
-  Action.preferences.server = server;
-  return;
+
+  Action.preferences.server = newServer;
 }
