f28503505c3dd9f4695e09aae727df5f0b7ea65b
Ptujec <derbenderchristian@gmail.com>
2025-02-19 13:45:35 +0100

    ARD Mediathek 1.1
    Updating suggestions to the new API URL.

--- Search-Actions/ARD Mediathek.lbaction/Contents/Scripts/suggestions.js
+++ Search-Actions/ARD Mediathek.lbaction/Contents/Scripts/suggestions.js
@@ -1,29 +1,17 @@
 // LaunchBar Action Script
 
 function runWithString(argument) {
-  if (argument == '') {
-    return;
-  }
-  var data = HTTP.getJSON(
-    'https://api.ardmediathek.de/search-system/mediathek/suggestions?query=' +
-      encodeURI(argument) +
-      '&resultCount=20'
+  if (!argument) return;
+
+  const data = HTTP.getJSON(
+    `https://api.ardmediathek.de/search-system/search/suggestions?query=${encodeURIComponent(
+      argument
+    )}&resultCount=20&platform=MEDIA_THEK`
   );
 
-  //   File.writeJSON(data, Action.supportPath + '/test.json');
-
-  var suggestions = [];
-  data.data.forEach(function (item) {
-    var pushData = {
-      title: item.title,
-      icon: 'iconTemplate',
-    };
-    if (item.type != 'Item' && item.type != '[null]') {
-      pushData.label = item.type;
-    }
-
-    suggestions.push(pushData);
-  });
-
-  return suggestions;
+  return data.data.map((item) => ({
+    title: item.title,
+    icon: 'iconTemplate',
+    ...(item.type !== 'Item' && item.type !== '[null]' && { label: item.type }),
+  }));
 }
