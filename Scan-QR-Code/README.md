dae836bfba354f85d9e9db5c45c9ba8ced9766c1
Ptujec <derbenderchristian@gmail.com>
2025-03-07 00:33:03 +0100

    Scan QR Code 1.0

---
+++ Scan-QR-Code/README.md
@@ -0,0 +1,27 @@
+# LaunchBar Action: Scan QR Code
+
+*[→ See a list of all my actions here.](https://ptujec.github.io/launchbar)* 
+
+This is a simple action that lets you scan QR codes. Launching the action will bring up [the standard screenshot UI](https://support.apple.com/en-us/102646#portion). 
+
+<img src="01.jpg" width="205"/> 
+
+To select the area of the screen with the QR code, click and hold your mouse or trackpad button while dragging the crosshair.
+
+<img src="02.jpg" width="205"/> 
+
+Instead of selecting an area, you can also select the entire window that contains the QR code by pressing the Space bar. The pointer changes to a camera icon. Now click the window to scan the code.
+
+<img src="03.jpg" width="205"/> 
+
+If there is just one QR code visible on your screen, you can hold `command` while launching the action to capture the entire screen for scanning the QR code.
+
+The action will send a notification that, depending on the QR code content, is clickable.
+
+## Setup (IMPORTANT!)
+
+**In order to run smoothly, actions written in Swift need to be both "un-quarantined" and compiled. I made [a dedicated action that does both](https://github.com/Ptujec/LaunchBar/tree/master/Compile-Swift-Action#readme). Run the `.lbaction` bundle of this action through the compile action before you start using it.**
+
+## Download & Update
+
+[Click here](https://github.com/Ptujec/LaunchBar/archive/refs/heads/master.zip) to download this LaunchBar action along with all the others. Or simply use [LaunchBar Repo Updates](https://github.com/Ptujec/LaunchBar/tree/master/LB-Repo-Updates#launchbar-repo-updates-action)! It helps automate updating existing and installing new actions.
---
+++ Scan-QR-Code/ScanQRCode.lbaction/Contents/Scripts/default.swift
@@ -0,0 +1,109 @@
+#!/usr/bin/env swift
+
+/*
+Scan QR Code Action for LaunchBar
+by Christian Bender (@ptujec)
+2025-03-07
+
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+*/
+
+import Cocoa
+import Vision
+
+struct Environment {
+    static let info = ProcessInfo.processInfo.environment
+    static let isCommandKeyPressed = info["LB_OPTION_COMMAND_KEY"] == "1"
+}
+
+enum QRContent {
+    case url(String)
+    case wifi(ssid: String, password: String)
+    case text(String)
+
+    init(_ content: String) {
+        if content.hasPrefix("WIFI:") {
+            let fields = Dictionary(
+                content.dropFirst(5).components(separatedBy: ";")
+                    .compactMap { component -> (String, String)? in
+                        let parts = component.split(separator: ":", maxSplits: 1)
+                        guard parts.count == 2 else { return nil }
+                        return (String(parts[0]), String(parts[1]).removingPercentEncoding ?? "")
+                    },
+                uniquingKeysWith: { $1 }
+            )
+            self = .wifi(ssid: fields["S"] ?? "", password: fields["P"] ?? "")
+        } else if content.hasPrefix("http") && URL(string: content) != nil {
+            self = .url(content)
+        } else {
+            self = .text(content)
+        }
+    }
+
+    var notification: (text: String, callback: String?) {
+        switch self {
+        case let .url(url):
+            return ("\(url)\nClick to open!", url)
+        case let .wifi(ssid, password):
+            return ("WiFi Network: \(ssid)\nPassword: \(password)\nCopied! Click to open Network Settings!",
+                    "x-apple.systempreferences:com.apple.preference.network")
+        case let .text(content):
+            return (content, nil)
+        }
+    }
+
+    var clipboardText: String {
+        switch self {
+        case let .wifi(_, password): password
+        case let .url(url): url
+        case let .text(content): content
+        }
+    }
+}
+
+func takeScreenshot() -> URL? {
+    let tempPath = (NSTemporaryDirectory() as NSString).appendingPathComponent("qr_screenshot.png")
+    let task = Process()
+    task.executableURL = URL(fileURLWithPath: "/usr/sbin/screencapture")
+    task.arguments = Environment.isCommandKeyPressed ? [tempPath] : ["-i", tempPath]
+    try? task.run()
+    task.waitUntilExit()
+    return URL(fileURLWithPath: tempPath)
+}
+
+func scanQRCode(from image: NSImage) -> String? {
+    guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return nil }
+    let request = VNDetectBarcodesRequest()
+    try? VNImageRequestHandler(cgImage: cgImage).perform([request])
+    return request.results?.first?.payloadStringValue
+}
+
+func notify(_ text: String, callback: String? = nil) {
+    let script = NSAppleScript(source: """
+    tell application "LaunchBar"
+        display in notification center "\(text)" with title "QR Code Content" \
+        \(callback.map { "callback URL \"\($0)\"" } ?? "")
+    end tell
+    """)
+    script?.executeAndReturnError(nil)
+}
+
+// MARK: Main execution
+
+if let screenshotURL = takeScreenshot() {
+    if let image = NSImage(contentsOf: screenshotURL) {
+        if let qrContent = scanQRCode(from: image) {
+            let content = QRContent(qrContent)
+            NSPasteboard.general.clearContents()
+            NSPasteboard.general.setString(content.clipboardText, forType: .string)
+
+            let notification = content.notification
+            notify(notification.text, callback: notification.callback)
+        } else {
+            notify("No QR code found in the selected area")
+        }
+    } else {
+        notify("Failed to process screenshot")
+    }
+    try? FileManager.default.removeItem(at: screenshotURL)
+}
