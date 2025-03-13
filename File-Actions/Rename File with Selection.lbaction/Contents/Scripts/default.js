f23692b8ef6a72bbd46919536ce6a9bf326dc650
Ptujec <derbenderchristian@gmail.com>
2025-03-13 22:48:34 +0100

    Rename File with Selection 1.1.1
    - refactor
    - fixing potential issues with extension extraction

--- File-Actions/Rename File with Selection.lbaction/Contents/Scripts/default.js
+++ File-Actions/Rename File with Selection.lbaction/Contents/Scripts/default.js
@@ -1,31 +1,24 @@
 /* 
-Rename File with Selection
+Rename File with Selection Action for LaunchBar
+by Christian Bender (@ptujec)
+2025-03-13
 
-https://www.satimage.fr/software/en/smile/external_codes/file_paths.html
-https://themacbeginner.com/rename-files-folders-mac-os-x-using-terminal/
-https://developer.obdev.at/launchbar-developer-documentation/#/javascript-file
-
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
 */
 
 function run(argument) {
-    argument = argument.trim()
+  const output = LaunchBar.executeAppleScript(
+    ` tell application (path to frontmost application as text)
+        set _path to file of document of window 1
+        set _path to POSIX path of _path
+      end tell`
+  ).trim();
 
-    var output = LaunchBar.executeAppleScript(
-        'tell application (path to frontmost application as text)',
-        'set _path to file of document of window 1',
-        'set _path to POSIX path of _path',
-        'end tell')
-        .trim()
+  const oldFile = output.trim();
+  const oldName = File.displayName(output);
+  const extension = oldName.split('.').pop();
+  const newName = argument.trim();
+  const newFile = output.replace(oldName, `${newName}.${extension}`);
 
-    var oldFile = output
-        .trim()
-
-    var oldName = File.displayName(output)
-    var ext = oldName.split('.')[1]
-
-    var re = new RegExp(oldName)
-    var newFile = output.replace(re, argument + '.' + ext)
-        .trim()
-
-    LaunchBar.execute('/bin/mv', oldFile, newFile)
+  LaunchBar.execute('/bin/mv', oldFile, newFile);
 }
