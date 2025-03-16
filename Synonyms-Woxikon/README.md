b12f8f6d185224af32409a0e81fb813791ceb172
Ptujec <derbenderchristian@gmail.com>
2025-03-16 21:47:55 +0100

    Synonyms (Woxikon) 1.0

---
+++ Synonyms-Woxikon/README.md
@@ -0,0 +1,17 @@
+# LaunchBar Action: Synonyms (Woxikon)
+
+This action displays synonyms from https://synonyme.woxikon.de/ in LaunchBar. The action currently supports German and English. 
+
+You can paste results by holding `shift` as usual or open the website with `command`.
+
+<img src="01.jpg" width="722"/>
+
+<img src="02.jpg" width="722"/>
+
+<img src="03.jpg" width="722"/>
+
+**Note:** There is no official API for Woxikon, so this action could break at any time.
+
+## Download & Update
+
+[Click here](https://github.com/Ptujec/LaunchBar/archive/refs/heads/master.zip) to download this LaunchBar action along with all the others. Or simply use [LaunchBar Repo Updates](https://github.com/Ptujec/LaunchBar/tree/master/LB-Repo-Updates#launchbar-repo-updates-action)! It helps automate updating existing and installing new actions.
---
+++ Synonyms-Woxikon/Synonyme.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,103 @@
+/* 
+Synonyme Action for LaunchBar 
+by Christian Bender (@ptujec)
+2022-12-30
+
+Copyright:
+Action: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+Source website: https://www.woxikon.de/nutzungsbedingungen
+*/
+
+function run(argument) {
+  const query = argument?.trim();
+  if (!query) return;
+
+  const getApiUrl = (lang) =>
+    `https://synonyme.woxikon.de/ajaxSearch?lang=${lang}&query=${encodeURI(
+      query
+    )}`;
+
+  const deData = HTTP.getJSON(getApiUrl('de')).data;
+  const enData = HTTP.getJSON(getApiUrl('en')).data;
+
+  const createResult = (item, lang) => ({
+    title: item.compat_word,
+    badge: lang,
+    icon: 'iconTemplate',
+    action: 'getSynonyms',
+    actionArgument: {
+      argument: item.compat_word,
+      url: `https://synonyme.woxikon.de/synonyme${
+        lang === 'en' ? '-englisch' : ''
+      }/${encodeURI(item.compat_word)}.php`,
+    },
+  });
+
+  const deResult = deData.map((item) => createResult(item, 'de'));
+  const enResult = enData.map((item) => createResult(item, 'en'));
+
+  return [...deResult, ...enResult];
+}
+
+function getSynonyms({ argument, url }) {
+  if (LaunchBar.options.commandKey) {
+    LaunchBar.openURL(url);
+    return;
+  }
+
+  const html = HTTP.loadRequest(url, {
+    timeout: 5.0,
+    method: 'GET',
+    resultType: 'text',
+  }).data;
+
+  // File.writeText(html, Action.supportPath + '/test2.html');
+  // return;
+  // const html = File.readText(`${Action.supportPath}/test2.html`);
+
+  const groups = html
+    .replace(/(\r\n|\n|\r|\s+)/gm, ' ')
+    .match(/<li class="synonyms-list-item.*?<\/li>/g);
+
+  File.writeText(groups.join('\n'), Action.supportPath + '/test.html');
+
+  const result = groups.map((group) => {
+    const meaning = group.match(/<b>(.*?)<\/b>/)?.[1] || '';
+    const synMatch =
+      group.match(/<div class="upper-synonyms">(.*?)<\/div>/)?.[1] || '';
+    const synonyms = synMatch
+      .replace(/<[^>]+>/g, '')
+      .split(/\s+/)
+      .filter((word) => word.length > 0);
+
+    return {
+      title: meaning,
+      subtitle: synonyms.join(' - '),
+      // label: argument,
+      alwaysShowsSubtitle: true,
+      icon: 'equalTemplate',
+      action: 'showSynonyms',
+      actionArgument: { synonyms, meaning, url },
+      actionReturnsItems: true,
+    };
+  });
+  return result;
+}
+
+function showSynonyms({ synonyms, meaning, url }) {
+  if (LaunchBar.options.commandKey) return LaunchBar.openURL(url);
+  if (LaunchBar.options.shiftKey) return LaunchBar.paste(meaning);
+
+  return synonyms.map((synonym) => ({
+    title: synonym,
+    label: meaning,
+    icon: 'equalTemplate',
+    action: 'doSomething',
+    actionArgument: { url, synonym },
+  }));
+}
+
+function doSomething({ url, synonym }) {
+  if (LaunchBar.options.shiftKey) return LaunchBar.paste(synonym);
+  LaunchBar.openURL(url);
+}
