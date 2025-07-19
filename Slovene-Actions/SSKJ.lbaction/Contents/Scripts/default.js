807d0ca2b0d0b2873a2520d3da1dc59b1d7c09de
Ptujec <derbenderchristian@gmail.com>
2025-07-19 12:50:32 +0200

    SSKJ² 1.1
    Refactored default.js for improved readability and modularity, extracting logic into helper functions and simplifying HTTP error handling. Updated suggestions.js to streamline code and use consistent icon naming. Info.plist updated to version 1.1 and removed author email and Twitter handle.

--- Slovene-Actions/SSKJ.lbaction/Contents/Scripts/default.js
+++ Slovene-Actions/SSKJ.lbaction/Contents/Scripts/default.js
@@ -1,220 +1,186 @@
-/* LaunchBar Action for SSKJ² 
+/* 
+LaunchBar Action for SSKJ² 
+by Christian Bender (@ptujec)
+2023-07-07
+Copyright see: https://github.com/Ptujec/LaunchBar/blob/master/LICENSE
+
+Website:
 https://www.fran.si/133/sskj2-slovar-slovenskega-knjiznega-jezika-2
 */
 
 function run(argument) {
-  var url =
-    'https://www.fran.si/iskanje?FilteredDictionaryIds=133&View=1&Query=' +
-    encodeURIComponent(argument);
+  const baseUrl = 'https://www.fran.si/iskanje';
+  const url = `${baseUrl}?FilteredDictionaryIds=133&View=1&Query=${encodeURIComponent(
+    argument
+  )}`;
 
   if (LaunchBar.options.commandKey) {
     LaunchBar.hide();
     LaunchBar.openURL(url);
-  } else {
-    var html = HTTP.loadRequest(url, {
-      timeout: 5.0,
-      method: 'GET',
-      resultType: 'text',
-    }).data;
+    return;
+  }
 
-    if (html != undefined) {
-      var groups = html.match(/<div class="entry-content">(.|\n|\r)*?<\/div>/g); //  list-group results
+  const data = HTTP.loadRequest(url, {
+    timeout: 5.0,
+    method: 'GET',
+    resultType: 'text',
+  });
 
-      if (groups == undefined) {
-        LaunchBar.hide();
-        LaunchBar.openURL(
-          'https://www.fran.si/iskanje?View=1&Query=' +
-            encodeURIComponent(argument) +
-            '&AllNoHeadword=' +
-            encodeURIComponent(argument) +
-            '&FilteredDictionaryIds=133'
-        );
-        return;
-      }
+  if (data.error) {
+    LaunchBar.alert('Error in HTTP request: ' + data.error);
+    return;
+  }
 
-      var groupsResult = [];
-      groups.forEach(function (item) {
-        var group = item
-          .toString()
-          .replace(/<span class="color_orange".*<\/ul>/g, '');
+  const html = data.data;
 
-        var header = group.match(
-          /<div class="entry-content">(.|\n|\r|\t)*?<br \/>/g
-        );
+  if (html) {
+    const groups = html.match(/<div class="entry-content">(.|\n|\r)*?<\/div>/g);
 
-        if (header == undefined) {
-          return;
-        }
-
-        header = header
-          .toString()
-          .replace(/(<([^>]+)>)/g, '')
-          .trim();
-
-        var meanings = group
-          .replace(/<br \/>/g, '<br />\n')
-          .match(
-            /(<span class="color_lightdark strong">.*?<\/span>)|(<span class="color_dark.*"explanation ?">.*?<\/span>)/g
-          )
-          .join('\n')
-          .replace(/(:)(\s?<\/)/g, '$2')
-          .replace(/(<([^>]+)>)/g, '')
-          .replace(/(^\d\..*)\n/gm, '$1')
-          .replace(/(^.\).*)\n/gm, '$1')
-          .replace(/\n(.\))/g, ' $1')
-          .replace(/(^\D)/gm, '/$1')
-          .replace(/\n\//g, ' / ')
-          .split('\n');
-
-        meanings.forEach(function (item) {
-          var title = item.replace(/(^\/)/, '');
-          var sub = header.replace(/(^\S+)\d+/, '$1');
-
-          var badge = header.match(/^\S+(\d+)/);
-
-          if (badge != undefined) {
-            badge = badge[1];
-          }
-
-          if (/^\d/.test(title)) {
-            if (badge != undefined) {
-              badge = badge + '.' + title.match(/^\d/);
-            } else {
-              badge = title.match(/^\d+/).toString();
-            }
-            title = title
-              .replace(/^\d+\./, '')
-              .trim()
-              .replace(/(^\/\s+)/, '');
-          }
-
-          var pushData = {
-            title: title,
-            subtitle: sub,
-            icon: 'resultTemplate',
-            url: url,
-          };
-
-          if (title.includes('/')) {
-            pushData.children = showParts(title, sub, url);
-          }
-
-          if (badge != undefined) {
-            pushData.badge = badge;
-          }
-
-          groupsResult.push(pushData);
-        });
-      });
-
-      var subGroups = html.match(/<span class="color_orange".*?<\/ul>/g);
-
-      if (subGroups != undefined) {
-        var subGroupsResult = [];
-        subGroups.forEach(function (item) {
-          var group = item.toString();
-
-          var header = group.match(/<span class="color_orange".*?<\/span>/g);
-
-          if (header == undefined) {
-            return;
-          }
-
-          header = header
-            .toString()
-            .replace(/(<([^>]+)>)/g, '')
-            .trim();
-
-          var meanings = group
-            .replace(/<br \/>/g, '<br />\n')
-            .match(
-              /(<span class="color_lightdark strong">.*?<\/span>)|(<span class="color_dark.*"explanation ?">.*?<\/span>)/g
-            );
-
-          if (meanings != undefined) {
-            meanings = meanings
-              .join('\n')
-              .replace(/(:)(\s?<\/)/g, '$2')
-              .replace(/(<([^>]+)>)/g, '')
-              .replace(/(^\d\..*)\n/gm, '$1')
-              .replace(/(^.\).*)\n/gm, '$1')
-              .replace(/\n(.\))/g, ' $1')
-              .replace(/(^\D)/gm, '/$1')
-              .replace(/\n\//g, ' / ')
-              .split('\n');
-
-            meanings.forEach(function (item) {
-              var title = item.replace(/(^\/)/, '');
-              var sub = header.replace(/(^\S+)\d+/, '$1');
-
-              var badge = header.match(/^\S+(\d+)/);
-
-              if (badge != undefined) {
-                badge = badge[1];
-              }
-
-              if (/^\d/.test(title)) {
-                if (badge != undefined) {
-                  badge = badge + '.' + title.match(/^\d/);
-                } else {
-                  badge = title.match(/^\d/).toString();
-                }
-                title = title.replace(/^\d\./, '').trim();
-              }
-
-              var pushData = {
-                title: title,
-                subtitle: sub,
-                icon: 'resultTemplate',
-                url: url,
-              };
-
-              if (title.includes('/')) {
-                pushData.children = showParts(title, sub, url);
-              }
-
-              if (badge != undefined) {
-                pushData.badge = badge;
-              }
-
-              subGroupsResult.push(pushData);
-            });
-          }
-        });
-        var result = groupsResult.concat(subGroupsResult);
-      } else {
-        var result = groupsResult;
-      }
-
-      return result;
-    } else {
-      var response = LaunchBar.alert(
-        'Timeout',
-        'The website took more than 5 seconds to launch. Do you want to try to open it in your browser?',
-        'Ok',
-        'Cancel'
+    if (!groups) {
+      LaunchBar.hide();
+      LaunchBar.openURL(
+        `${baseUrl}?View=1&Query=${encodeURIComponent(
+          argument
+        )}&AllNoHeadword=${encodeURIComponent(
+          argument
+        )}&FilteredDictionaryIds=133`
       );
-      switch (response) {
-        case 0:
-          LaunchBar.openURL(url);
-        case 1:
-          break;
-      }
+      return;
     }
+
+    const groupsResult = processGroups(groups, url);
+    const subGroups = html.match(/<span class="color_orange".*?<\/ul>/g);
+
+    if (!subGroups) {
+      return groupsResult;
+    }
+
+    const subGroupsResult = processSubGroups(subGroups, url);
+    return groupsResult.concat(subGroupsResult);
   }
 }
 
-function showParts(title, sub, url) {
-  var parts = title.split('/');
+function processGroups(groups, url) {
+  const results = [];
 
-  var result = [];
-  parts.forEach(function (item) {
-    result.push({
-      title: item.trim(),
+  groups.forEach((group) => {
+    const cleanGroup = group
+      .toString()
+      .replace(/<span class="color_orange".*<\/ul>/g, '');
+    const header = extractHeader(cleanGroup);
+
+    if (!header) return;
+
+    const meanings = extractMeanings(cleanGroup);
+    processMeanings(meanings, header, results, url);
+  });
+
+  return results;
+}
+
+function processSubGroups(subGroups, url) {
+  const results = [];
+
+  subGroups.forEach((group) => {
+    const header = extractSubGroupHeader(group);
+    if (!header) return;
+
+    const meanings = extractMeanings(group.toString());
+    if (meanings) {
+      processMeanings(meanings, header, results, url);
+    }
+  });
+
+  return results;
+}
+
+function extractHeader(group) {
+  const headerMatch = group.match(
+    /<div class="entry-content">(.|\n|\r|\t)*?<br \/>/g
+  );
+  return headerMatch?.map((h) =>
+    h
+      .toString()
+      .replace(/(<([^>]+)>)/g, '')
+      .trim()
+  )[0];
+}
+
+function extractSubGroupHeader(group) {
+  const headerMatch = group.match(/<span class="color_orange".*?<\/span>/g);
+  return headerMatch?.map((h) =>
+    h
+      .toString()
+      .replace(/(<([^>]+)>)/g, '')
+      .trim()
+  )[0];
+}
+
+function extractMeanings(text) {
+  return text
+    .replace(/<br \/>/g, '<br />\n')
+    .match(
+      /(<span class="color_lightdark strong">.*?<\/span>)|(<span class="color_dark.*"explanation ?">.*?<\/span>)/g
+    )
+    ?.join('\n')
+    .replace(/(:)(\s?<\/)/g, '$2')
+    .replace(/(<([^>]+)>)/g, '')
+    .replace(/(^\d\..*)\n/gm, '$1')
+    .replace(/(^.\).*)\n/gm, '$1')
+    .replace(/\n(.\))/g, ' $1')
+    .replace(/(^\D)/gm, '/$1')
+    .replace(/\n\//g, ' / ')
+    .split('\n');
+}
+
+function processMeanings(meanings, header, results, url) {
+  meanings.forEach((item) => {
+    let title = item.replace(/(^\/)/, '');
+    const sub = header.replace(/(^\S+)\d+/, '$1');
+    const badgeMatch = header.match(/^\S+(\d+)/);
+    let badge = badgeMatch?.[1];
+
+    if (/^\d/.test(title)) {
+      if (badge) {
+        badge = `${badge}.${title.match(/^\d/)}`;
+      } else {
+        badge = title.match(/^\d+/).toString();
+      }
+    }
+
+    // Remove numbers after capturing them for the badge
+    title = title.replace(/^\d+\.\s*/, '');
+
+    const result = {
+      title: title.trim().replace(/(^\/\s+)/, ''),
       subtitle: sub,
       icon: 'resultTemplate',
-      url: url,
-    });
+      action: 'openURL',
+      actionArgument: url,
+    };
+
+    if (title.includes('/')) {
+      result.children = showParts(title, sub, url);
+    }
+
+    if (badge) {
+      result.badge = badge;
+    }
+
+    results.push(result);
   });
-  return result;
+}
+
+function showParts(title, sub, url) {
+  return title.split('/').map((item) => ({
+    title: item.trim(),
+    subtitle: sub,
+    icon: 'resultTemplate',
+    action: 'openURL',
+    actionArgument: url,
+  }));
+}
+
+function openURL(url) {
+  LaunchBar.openURL(url);
 }
--- Slovene-Actions/SSKJ.lbaction/Contents/Scripts/suggestions.js
+++ Slovene-Actions/SSKJ.lbaction/Contents/Scripts/suggestions.js
@@ -1,38 +1,30 @@
 function runWithString(argument) {
-  if (argument != undefined) {
-    argument = argument.trim();
-    if (argument != '') {
-      var result = HTTP.getJSON(
-        'http://www.fran.si/ajax/iskanje/autocomplete?query=' +
-          encodeURIComponent(argument) +
-          '&dictionaries=133',
-        3
-      );
+  if (!argument?.trim()) return [];
 
-      if (result == undefined) {
-        LaunchBar.log('HTTP.getJSON() returned undefined');
-        return [];
-      }
-      if (result.error != undefined) {
-        LaunchBar.log('Error in HTTP request: ' + result.error);
-        return [];
-      }
+  const result = HTTP.getJSON(
+    'http://www.fran.si/ajax/iskanje/autocomplete?query=' +
+      encodeURIComponent(argument.trim()) +
+      '&dictionaries=133',
+    3
+  );
 
-      try {
-        var suggestions = [];
-        var i = 0;
-        for (i = 0; i < result.data.length; i++) {
-          var suggestion = result.data[i];
-          suggestions.push({
-            title: suggestion,
-            icon: 'sTemplate.png',
-          });
-        }
-        return suggestions;
-      } catch (exception) {
-        LaunchBar.log('Exception while parsing result: ' + exception);
-        return [];
-      }
-    }
+  if (!result) {
+    LaunchBar.log('HTTP.getJSON() returned undefined');
+    return [];
+  }
+
+  if (result.error) {
+    LaunchBar.log('Error in HTTP request: ' + result.error);
+    return [];
+  }
+
+  try {
+    return result.data.map((suggestion) => ({
+      title: suggestion,
+      icon: 'resultTemplate',
+    }));
+  } catch (exception) {
+    LaunchBar.log('Exception while parsing result: ' + exception);
+    return [];
   }
 }
