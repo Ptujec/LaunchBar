e22914cb3072d179c8545a237b5abff81be19a2f
Ptujec <derbenderchristian@gmail.com>
2026-05-02 13:06:09 +0200

    Accordance - Paste scripture 3.4
    added special treatment for pasting into iA Writer and Keynote

--- Accordance-Actions/Accordance - Paste scripture.lbaction/Contents/Scripts/default.js
+++ Accordance-Actions/Accordance - Paste scripture.lbaction/Contents/Scripts/default.js
@@ -9,16 +9,17 @@ Sources:
 - https://developer.obdev.at/launchbar-developer-documentation/#/javascript-launchbar
 - http://macbiblioblog.blogspot.com/2009/01/downloads.html
 
+TODO: Refactor (functions and such)
 */
 
 String.prototype.localizationTable = 'default';
 
 const AccordancePrefs = eval(
-  File.readText('~/Library/Preferences/Accordance Preferences/General.apref')
+  File.readText('~/Library/Preferences/Accordance Preferences/General.apref'),
 )[0]; // For default translation & vers notation settings
 
 const bookNameDictionary = File.readJSON(
-  Action.path + '/Contents/Resources/booknames.json'
+  Action.path + '/Contents/Resources/booknames.json',
 ); // Currently contains German and Slovene names. You could expand it with your language by adding the relevant names to the "alt" array.
 
 const textModulesPath =
@@ -58,7 +59,7 @@ function run(argument) {
     // convert book names
     newArgument = argument.replace(
       /([a-zžščöäüß]+(?: [a-zžščöäüß]+)?)/gi,
-      (match, p1) => (p1 != 'f' && p1 != 'ff' ? replaceBookName(p1) : p1)
+      (match, p1) => (p1 != 'f' && p1 != 'ff' ? replaceBookName(p1) : p1),
     );
 
     // more clean up for accordance and special treatment for the pentateuch
@@ -75,25 +76,20 @@ function run(argument) {
   if (LaunchBar.options.commandKey) {
     return listTranslations(newArgument, argument);
   } else {
-    pasteText(newArgument, argument, translation);
+    getText(newArgument, argument, translation);
   }
 }
 
-function pasteText(newArgument, argument, translation) {
-  var text = LaunchBar.executeAppleScript(
-    'tell application "Accordance" to set theResult to «event AccdTxRf» {"' +
-      translation +
-      '", "' +
-      newArgument +
-      '", true}'
+function getText(newArgument, argument, translation) {
+  let text = LaunchBar.executeAppleScript(
+    `tell application "Accordance" to set theResult to «event AccdTxRf» {"${translation}", "${newArgument}", true}`,
   ).trim();
 
+  // TODO: Pick a different translation
+  // return listTranslations(newArgument, argument);
   if (text.startsWith('ERR')) {
     LaunchBar.alert('Error!', text);
-    // var output = listTranslations(newArgument, argument);
-    // return output;
     return;
-    // TODO: Pick a different translation
   }
 
   // Cleanup quote (Ony works if you have checked "Split discontiguous verses" in Citation settings)
@@ -104,16 +100,89 @@ function pasteText(newArgument, argument, translation) {
   // Uppercase first character of query
   argument = argument.charAt(0).toUpperCase() + argument.slice(1);
 
-  let format = Action.preferences.format;
-  let defaultText = text + ' (' + argument + ' ' + translationName + ')';
+  const reference = `${argument} ${translationName}`;
 
-  if (format == 'markdown') {
-    LaunchBar.paste('> ' + defaultText);
-  } else if (format == 'citation') {
-    LaunchBar.paste('„' + text + '“ ' + argument + ' ' + translationName);
-  } else {
-    LaunchBar.paste(defaultText);
+  paste(text, reference);
+}
+
+function paste(text, reference) {
+  LaunchBar.hide();
+
+  let format = Action.preferences.format;
+  let formattedText = `${text} (${reference})`;
+
+  formattedText =
+    format === 'markdown'
+      ? `> ${formattedText}`
+      : format === 'citation'
+        ? `"${text}" ${reference}`
+        : formattedText;
+
+  const getFrontmostAS =
+    'tell application "System Events" to set _frontmoste to bundle identifier of application processes whose frontmost is true as string';
+
+  const frontmostAppID = LaunchBar.executeAppleScript(getFrontmostAS).trim();
+
+  if (frontmostAppID === 'pro.writer.mac') {
+    return pasteInWriter(formattedText);
   }
+
+  if (frontmostAppID === 'com.apple.iWork.Keynote') {
+    return pasteInKeynote(text, reference);
+  }
+
+  // TODO: Add location url to the clipboard (accord://read/ELBER#Rom_4,11) … Accordance Paste Helper Action
+  return LaunchBar.paste(formattedText);
+}
+
+function pasteInWriter(text) {
+  LaunchBar.setClipboardString(text);
+
+  // TODO: Implement in Settings
+  // const authorName = Action.preferences.iaAuthor;
+  const authorName = 'Accordance';
+  const pasteEditsFromMenu =
+    'menu 1 of menu item 10 of menu 4 of menu bar 1 of application process "iA Writer"';
+
+  const pasteInWriterAS =
+    'delay 0.1\n' +
+    'tell application "iA Writer" to activate\n' +
+    'tell application "System Events"\n' +
+    `click menu item "${authorName}" of ${pasteEditsFromMenu}\n` +
+    'end tell\n';
+
+  LaunchBar.executeAppleScript(pasteInWriterAS);
+}
+
+function pasteInKeynote(text, reference) {
+  // Implement
+
+  LaunchBar.executeAppleScript(`
+      tell application "Keynote"
+      	activate
+      	set _doc to the front document
+        set _slide_num to slide number of current slide of _doc
+	      tell _doc to duplicate slide _slide_num to after slide _slide_num
+      	set _slide to current slide of _doc      	
+      	tell _slide
+          set _text_items to get text items          
+          -- set text values
+          set object text of item 1 of _text_items to "${text}"
+      		set object text of item 2 of _text_items to "${reference}"
+          -- set position of reference 20 below the text
+      		delay 0.1
+      		
+      		set _pos_1 to get position of item 1 of _text_items
+      		set _y_1 to item 2 of _pos_1
+      		set _height_1 to get height of item 1 of _text_items
+      		
+      		set _y_2 to _y_1 + _height_1 + 20
+      		set _pos_2 to get position of item 2 of _text_items
+      		set _x_2 to item 1 of _pos_2
+      		set position of item 2 of _text_items to {_x_2, _y_2}
+        end tell   	
+      end tell
+    `);
 }
 
 function replaceBookName(bookName) {
@@ -143,8 +212,8 @@ function settings() {
     Action.preferences.format == 'plain'
       ? 'plainTemplate'
       : Action.preferences.format == 'citation'
-      ? 'citationTemplate'
-      : 'markdownTemplate';
+        ? 'citationTemplate'
+        : 'markdownTemplate';
 
   return [
     {
@@ -263,5 +332,5 @@ function setDefaultTranslation({ translation }) {
 
 function setTranslation({ newArgument, argument, translation }) {
   Action.preferences.lastUsed = translation;
-  pasteText(newArgument, argument, translation);
+  getText(newArgument, argument, translation);
 }
